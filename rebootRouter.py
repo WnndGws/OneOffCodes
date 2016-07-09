b6ed94586f4b03d7ba0a27ddbbb53bceedca3ac5
WnndGws <wynandgouwswg@gmail.com>
2016-07-09 12:04:43 +0800

    Delete rebootRouter.py

--- rebootRouter.py
+++
@@ -1,9 +0,0 @@
-from grab import Grab
-
-g = Grab()
-pw = input('What is your password? ')
-
-g.go('http://192.168.0.1/html/reboot.html')
-g.doc.set_input('username', 'admin')
-g.doc.set_input('password', pw)
-g.doc.submit()
