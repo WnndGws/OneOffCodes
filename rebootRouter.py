cf438df112de3ecc6011d3f9d90ec9a8754f9885
wnndgws <wynand@gouws.com.au>
2016-07-09 11:41:15 +0800

    Changed pw to input

--- rebootRouter.py
+++ rebootRouter.py
@@ -1,8 +1,9 @@
 from grab import Grab
 
 g = Grab()
+pw = input('What is your password? ')
 
 g.go('http://192.168.0.1/html/reboot.html')
 g.doc.set_input('username', 'admin')
-g.doc.set_input('password', 'addLater')
+g.doc.set_input('password', pw)
 g.doc.submit()
