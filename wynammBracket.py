0d0c0b794602bbb4f97d9d2568ade76381f8bb85
ammgws <zilla.icles@gmail.com>
2016-07-11 22:10:46 +0900

    moved possible team list to text file, more
    - moved possible team list to text file
    - read text file in and shuffle the resulting list
    - take the first 16 shuffled ('random') teams for wyn, the next 16 for amm
    - go through each list 2 at a time and have user choose team
    - create dict of matchups ('opponents')

--- wynammBracket.py
+++ wynammBracket.py
@@ -7,26 +7,56 @@
 5) repeat'''
 
 import random
+import sys
 
-opponentsDict = {}
-scoresDict = {}
+def team_chooser(team_list):
+    '''Team chooser helper function.Enter '1' to select first displayed team, and '2' or anything other than '1' for second team'''
+    chosen_teams = []
+    for i, team in enumerate(team_list):
+        if i % 2 == 0:
+            selection = input("Enter 1 for {0} or 2 for {1}: ".format(team_list[i], team_list[i+1]))
+            if selection == '1':
+                chosen_teams.append(team_list[i])
+            else:
+                chosen_teams.append(team_list[i+1])
+    return chosen_teams
 
-possibleTeams = ['FC Bayern', 'FC Barcelona', 'Real Madrid', 'PSG', 'Chelsea', 'Man City', 'Arsenal', 'Juventus',
-                 'Bor Dortmund', 'Atletico Madrid', 'Man Utd.', 'Valencia', 'Napoli', 'Spurs', 'Liverpool', 'Roma', 'Sevilla FC',
-                 'Villarreal CF', 'Bayer 04', 'Vfl Wolfsburg', 'Inter', 'Athletic Bilbao', 'SL Benfica', 'Sporting CP', 'Zenit',
-                 'FC Schalke 04', 'Milan', 'Everton', 'Lazio', 'FC Porto', "Bor. M'gladbach", 'Besiktas', 'Fenerbahce' 'Fiorentina',
-                 'Olym Lyonnais', 'AS Monaco', 'Real Sociedad', 'Newcastle Utd.', 'Stoke', 'West Ham', 'Celta Vigo', 'Swansea', 
-                 'Olym. Marseille', 'Torino', 'Real Betis', 'Leicester City', 'Southampton', 'Malaga CF', 'Crystal Palace', 'PSV',
-                 '1899 Hoffenheim', 'Shakhtar Donetsk', 'Ajax', 'AS Saint-Etienne', 'CSKA Moscow', 'RC Deportivo', 'Sunderland',
-                 'Watford', 'Stade Rennais', 'RCD Espanyol']
-           
-shuffledTeams = []
-for item in possibleTeams:
-    shuffledTeams.append(item)
-random.shuffle(shuffledTeams)
+# Get absolute path of the dir script is run from
+cwd = sys.path[0]
 
-#next step: present each with two teams, and save the choice to teams list, and the matchup to opponentsDict
-                 
-wynTeams = []
-ammTeams = []
+# Set colors for terminal output
+colour_red = "\033[01;31m{0}\033[00m"
+colour_green = "\033[1;36m{0}\033[00m"
 
+matchups_dict = {}
+scores_dict = {}
+
+with open(cwd + '/wynnammBracket_possible_teams.txt', 'r') as file:
+    possible_teams=file.read().splitlines()
+random.shuffle(possible_teams) # Randomise teams
+
+if len(possible_teams) < 32:
+    raise Exception("Minimum of 32 teams required in text file")
+
+# Create random list of 16 teams each
+possible_wyn_teams = possible_teams[:16]
+possible_amm_teams = possible_teams[16:32]
+
+# Present 2 teams at a time and have each choose their 8 teams
+chosen_wyn_teams = []
+chosen_amm_teams = []
+print(colour_red.format('***Choosing Wyn teams***'))
+chosen_wyn_teams = team_chooser(possible_wyn_teams)
+print(colour_red.format('***Choosing Amm teams***'))
+chosen_amm_teams = team_chooser(possible_amm_teams)
+
+print('*****************************')
+print(chosen_wyn_teams)
+print('*****************************')
+print(chosen_amm_teams)
+
+# Create matchups: wyn team 1 vs amm team 1 etc
+for i, team in enumerate(chosen_wyn_teams):
+    matchups_dict[team] = chosen_amm_teams[i]
+    
+print(colour_green.format(matchups_dict))
