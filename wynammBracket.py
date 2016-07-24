ca91e6bfd7076934cd23b7d900b30882f85703dc
ammgws <ammgws@users.noreply.github.com>
2016-07-24 17:30:52 +0900

    added save to file, trying to add print tournament function

--- wynammBracket.py
+++ wynammBracket.py
@@ -1,107 +1,173 @@
-'''Tasks:
-1) save it all in a dictionary?
-2) present us each with 2 options for teams
-3) once 16 teams (8 each) have been chosen, progress to second round
-    a) print out score from 1st round so we know who won etc.
-4) once 2 legs have been played, halve the bracket, if one player has more than 4 teams go through they choose 1 team to keep and 1 team to eliminate
-5) repeat'''
+'''wynammBracket - script to setup and track fef tournaments'''
 
-import random
-import os
+# To do:
+# once 16 teams (8 each) have been chosen, progress to second round
+# print out score from 1st round so we know who won etc.
+# once 2 legs have been played, halve the bracket, if one player has more
+# than 4 teams go through they choose 1 team to keep and 1 team to eliminate
+# make this into a Class object
+
+# Imports from Python Standard Library
+import os.path
+import pickle
 import sys
+from random import shuffle
 
 # Set colors for terminal output
-colour_red = "\033[01;31m{0}\033[00m"
-colour_green = "\033[1;36m{0}\033[00m"
+colour_red = "\033[01;31m{0}\033[00m"  # Wyn colour
+colour_green = "\033[1;36m{0}\033[00m"  # Amm colour
 
-def team_chooser(team_list):
-    '''Team chooser helper function. Enter '1' to select first displayed team, and '2' or anything other than '1' for second team'''
-    chosen_teams = []
-    i = 0  #using this so that we can print the same teams if incorrect entry
-    print ('Press either "1" or "2" to choose your team')
-    selection = input("(1) {0}\n(2) {1}: \nChoice: ".format(team_list[i], team_list[i+1]))
-    if selection not in ('1','2'):
-        print ('Please only enter 1 or 2')     
-    elif selection == '1':
-        chosen_teams.append(team_list[i])
-    elif selection == '2':
-        chosen_teams.append(team_list[i+1])
-        i += 2
-                                
-    return chosen_teams
-    
+
 def load_teams():
-    cwd = sys.path[0] # Get absolute path of the dir script resides in. os.getcwd() only returns dir script is invoked from
-    if os.path.isfile(cwd + '/wynammBracket_possible_teams.txt'):        
+    '''Load teams from file, otherwise load default teams.'''
+    # Get absolute path of the dir script resides in.
+    # os.getcwd() only returns dir script is invoked from.
+    cwd = sys.path[0]
+    if os.path.isfile(cwd + '/wynammBracket_possible_teams.txt'):
         with open(cwd + '/wynammBracket_possible_teams.txt', 'r') as file:
-            possible_teams = file.read().splitlines()
-        random.shuffle(possible_teams) # Randomly order the teams
-        return possible_teams
-    else: #add default values when no config file is found
-        print (colour_red.format('***NO TEAMS LIST FILE FOUND***'))
-        print ('Loading default teams.........')
-        possible_teams = ['FC Bayern', 'FC Barcelona', 'Real Madrid', 'PSG', 'Chelsea', 'Man City', 'Arsenal', 'Juventus',
-                 'Bor Dortmund', 'Atletico Madrid', 'Man Utd.', 'Valencia', 'Napoli', 'Spurs', 'Liverpool', 'Roma', 'Sevilla FC',
-                 'Villarreal CF', 'Bayer 04', 'Vfl Wolfsburg', 'Inter', 'Athletic Bilbao', 'SL Benfica', 'Sporting CP', 'Zenit',
-                 'FC Schalke 04', 'Milan', 'Everton', 'Lazio', 'FC Porto', "Bor. M'gladbach", 'Besiktas', 'Fenerbahce', 'Fiorentina',
-                 'Olym Lyonnais', 'AS Monaco', 'Real Sociedad', 'Newcastle Utd.', 'Stoke', 'West Ham', 'Celta Vigo', 'Swansea', 
-                 'Olym. Marseille', 'Torino', 'Real Betis', 'Leicester City', 'Southampton', 'Malaga CF', 'Crystal Palace', 'PSV',
-                 '1899 Hoffenheim', 'Shakhtar Donetsk', 'Ajax', 'AS Saint-Etienne', 'CSKA Moscow', 'RC Deportivo', 'Sunderland',
-                 'Watford', 'Stade Rennais', 'RCD Espanyol']
-        random.shuffle(possible_teams)
-        return possible_teams
+            teams = file.read().splitlines()
+    else:  # Add default values when no config file is found
+        print(colour_red.format('***NO TEAMS LIST FILE FOUND***'))
+        print('Loading default teams.........')
+        teams = ['FC Bayern', 'FC Barcelona', 'Real Madrid', 'PSG', 'Chelsea',
+                 'Man City', 'Arsenal', 'Juventus', 'Bor Dortmund',
+                 'Atletico Madrid', 'Man Utd.', 'Valencia', 'Napoli', 'Spurs',
+                 'Liverpool', 'Roma', 'Sevilla FC', 'Villarreal CF', 'Bayer 04',
+                 'Vfl Wolfsburg', 'Inter', 'Athletic Bilbao', 'SL Benfica',
+                 'Sporting CP', 'Zenit', 'FC Schalke 04', 'Milan', 'Everton',
+                 'Lazio', 'FC Porto', "Bor. M'gladbach", 'Besiktas', 'Fenerbahce',
+                 'Fiorentina', 'Olym Lyonnais', 'AS Monaco', 'Real Sociedad',
+                 'Newcastle Utd.', 'Stoke', 'West Ham', 'Celta Vigo', 'Swansea',
+                 'Olym. Marseille', 'Torino', 'Real Betis', 'Leicester City',
+                 'Southampton', 'Malaga CF', 'Crystal Palace', 'PSV',
+                 '1899 Hoffenheim', 'Shakhtar Donetsk', 'Ajax', 'AS Saint-Etienne',
+                 'CSKA Moscow', 'RC Deportivo', 'Sunderland', 'Watford',
+                 'Stade Rennais', 'RCD Espanyol']
+    shuffle(teams)  # Randomly order the teams
+    return teams
 
 
 def team_list_edit(teams):
-    ''' Step through default teams list team by team and ask whether to remove or keep '''
+    ''' Step through default teams list and ask whether to remove or keep.'''
     num_teams = len(teams)
     for i, team in enumerate(teams):
-        user_input = input("{0}/{1}th team: {2}. Press any key to keep, 'r' to remove, 'e' to exit: ".format(i+1, num_teams, team))
-        if user_input == 'r':
+        user_input = input('Team no {0}/{1}: {2}.\n'
+                           '(1) Keep\n'
+                           '(2) Edit\n'
+                           '(3) Remove\n'
+                           '(4) Exit\n'
+                           'Choice:'.format(i+1, num_teams, team))
+        if user_input == '3':
             teams.remove(team)
             num_teams -= 1
-        elif user_input == 'e':
-            break
+        elif user_input == '4':
+            return teams
 
-    if len(possible_teams) < 32:
-        print("Warning: team list only has {0} teams. Need 32 or more!".format(len(teams)))
+    return teams
+
 
 def team_list_add(teams):
-    ''' Add team to default team list '''
+    ''' Add team to default team list.'''
     user_input = input("Type team name, or 'e' to exit: ")
     if user_input == 'e':
         return teams
     else:
         teams.append(user_input)
-        
-    return teams  
-    
-if __name__ == '__main__':
-    # Set colors for terminal output
-    colour_red = "\033[01;31m{0}\033[00m"
-    colour_green = "\033[1;36m{0}\033[00m"
 
+    return teams
+
+
+def team_chooser(team_list):
+    '''Team chooser helper function.'''
+    chosen_teams = []
+    for i, team in enumerate(team_list):
+        if i % 2 == 0:
+            while True:
+                selection = input('Press either "1" or "2" to choose your team\n'
+                                  '(1) {0}\n'
+                                  '(2) {1}\n'
+                                  'Choice:'.format(team_list[i], team_list[i+1]))
+                if selection not in ('1', '2'):
+                    continue
+                elif selection == '1':
+                    chosen_teams.append(team_list[i])
+                    break
+                elif selection == '2':
+                    chosen_teams.append(team_list[i+1])
+                    break
+    return chosen_teams
+
+
+def save_tournament(matchups_dict, scores_dict):
+    ''' Save tournament to file.'''
+    with open('fef.pickle', 'wb') as file:
+        pickle.dump((matchups_dict, scores_dict),
+                    file,
+                    protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def load_tournament(matchups_dict, scores_dict):
+    ''' Load tournament from file.'''
+    with open('fef.pickle', 'rb') as file:
+        matchups_dict, scores_dict = pickle.load(file)
+
+
+def calc_spaces(wyn_team, amm_team):
+    ''' Calculate no. of spaces to put after team name when printing tournament.'''
+    length_diff = len(wyn_team) - len(amm_team)
+
+    if len(wyn_team) > len(amm_team):
+        wyn_spaces = ' '
+        amm_spaces = ' ' * length_diff
+    else:
+        wyn_spaces = ' ' * length_diff
+        amm_spaces = ' '
+
+    return wyn_spaces, amm_spaces
+
+
+def print_tournament(matchups_dict, scores_dict):
+    ''' Print tournament bracket into terminal.'''
+    matchup_number = 0
+    while matchup_number < len(matchups_dict) / 2:
+        wyn_team = list(matchups_dict)[matchup_number]
+        amm_team = matchups_dict[list(matchups_dict)[matchup_number]]
+        wyn_spaces, amm_spaces = calc_spaces(wyn_team, amm_team)
+        print(colour_red.format('{1}{2}---\\\n'.format(wyn_team, wyn_spaces)))
+        print(colour_green.format('{1}{2}---/\n'.format(amm_team, amm_spaces)))
+        matchup_number += 1
+
+
+def main():
+    ''' Main function.'''
     matchups_dict = {}
     scores_dict = {}
-    
+
     possible_teams = load_teams()
-    print("mvy fef tournament-o-matic.\nLoaded the following teams: \n")
+#    print("mvy fef tournament-o-matic.\nLoaded the following teams: \n")
 #    print(possible_teams)
-    
-    while True: # Keep looping until user explicitly chooses to proceed. Allows user as many chances as they want to edit/add teams
-        user_input = input("(1) Proceed\n(2) Edit teams\n(3) Add teams\n(4) Print current teams\nChoice: ")
+
+    while True:
+        # Keep looping until user explicitly chooses to proceed.
+        # Allows user as many chances as they want to edit/add teams.
+        user_input = input('(1) Proceed\n'
+                           '(2) Edit teams\n'
+                           '(3) Add teams\n'
+                           '(4) Print current teams\n'
+                           'Choice: ')
         if user_input == '1':
-            break
+            if len(possible_teams) < 32:
+                print('Minimum of 32 teams required to proceed!')
+            else:
+                break
         elif user_input == '2':
             possible_teams = team_list_edit(possible_teams)
         elif user_input == '3':
             possible_teams = team_list_add(possible_teams)
         elif user_input == '4':
+            print('No. teams: {0}'.format(len(possible_teams)))
             print(possible_teams)
-        
-    if len(possible_teams) < 32:
-        raise Exception("Minimum of 32 teams required in default teams text file!")
-            
+
     # Create random list of 16 teams each
     possible_wyn_teams = possible_teams[:16]
     possible_amm_teams = possible_teams[16:32]
@@ -111,15 +177,20 @@ if __name__ == '__main__':
     chosen_amm_teams = []
     print(colour_red.format("***Choosing Wyn teams***"))
     chosen_wyn_teams = team_chooser(possible_wyn_teams)
-    print(colour_green.format("***Choosing Amm teams***")) #use different colours for differnt users
+    print(colour_green.format("***Choosing Amm teams***"))
     chosen_amm_teams = team_chooser(possible_amm_teams)
 
     # Create matchups: wyn team 1 vs amm team 1 etc
     for i, team in enumerate(chosen_wyn_teams):
         matchups_dict[team] = chosen_amm_teams[i]
-    
-    matchup_number = 0    
-    print(colour_green.format("Matchup number {0}: {1}(Wyn) vs. {2}(Amm)".format(matchup_number+1, list(matchups_dict)[matchup_number], matchups_dict[list(matchups_dict)[matchup_number]])))
-    matchup_number += 1
-    
-    '''Next thing to do is dump matchup_dict to a pickle, and then let it loop through all teams until all teams gone'''
+        print(colour_green.format('Matchup number {0}: '
+                                  '{1} (Wyn) vs. {2} (Amm)'.format(i+1,
+                                                                   team,
+                                                                   chosen_amm_teams[i])))
+    # Save tournament to file
+    save_tournament(matchups_dict, scores_dict)
+    # Print tournament status to terminal
+    print_tournament(matchups_dict, scores_dict)
+
+if __name__ == '__main__':
+    main()
