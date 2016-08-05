e3b8045b6898351d3dabbebf224a26e3aab5e6a1
ammgws <ammgws@users.noreply.github.com>
2016-08-05 23:27:22 +0900

    cleaning up

--- bisectionMethod.py
+++ bisectionMethod.py
@@ -12,7 +12,7 @@ import numpy as np
 import parser
 
 
-def graph(formula, xmin, xmax):
+def graph(equation, xmin, xmax):
     xTicks = (xmax - xmin)/200
     x = np.arange(xmin, xmax, xTicks)
     y = eval(equation)
@@ -31,74 +31,78 @@ def graph(formula, xmin, xmax):
     ax.axvline(linewidth=2, color='blue')
     pyplot.show()
 
-# First build the equation based on user input
-coef = input("Please enter all coefficients of your equation (including zeros!) seperated by commas (eg. x^2 would be input as 1,0,0): ")
-coef = list(map(float, coef.split(',')))
-terms = len(coef)
-equation = ""
-params = 0
-while params < terms:
-    equation = equation + str("+" + str(coef[params]) + "*x**" + str(terms-params-1))
-    params = params+1
 
-# Plot the function
-graph(equation, -100, 100)
+def main():
+    # First build the equation based on user input
+    coef = input("Please enter all coefficients of your equation (including zeros!) seperated by commas (eg. x^2 would be input as 1,0,0): ")
+    coef = list(map(float, coef.split(',')))
+    terms = len(coef)
+    equation = ""
+    params = 0
+    while params < terms:
+        equation = equation + str("+" + str(coef[params]) + "*x**" + str(terms-params-1))
+        params = params+1
 
-# If zooming in, ask for the x-axis interval to zoom-in to
-happy = 'no'
-while happy == 'no':
-    zoom = str(input('Would you like to zoom in further? (Yes/No): '))
-    allowedZoom = ["yes", "y", "no", "n"]
-    if zoom.lower() not in allowedZoom:
-        print("Please enter only 'Yes' or 'No'")
-        happy = 'no'
-    elif ('y' or 'Y') in zoom:
-        xmin = int(input('Enter x-min: '))
-        xmax = int(input('Enter x-max: '))
-        if xmin >= xmax:
-            print("Make sure that xmin is lower than xmax")
+    # Plot the function
+    graph(equation, -100, 100)
+
+    # If zooming in, ask for the x-axis interval to zoom-in to
+    happy = 'no'
+    while happy == 'no':
+        zoom = str(input('Would you like to zoom in further? (Yes/No): '))
+        allowedZoom = ["yes", "y", "no", "n"]
+        if zoom.lower() not in allowedZoom:
+            print("Please enter only 'Yes' or 'No'")
+            happy = 'no'
+        elif zoom.lower() == 'y':
+            xmin = int(input('Enter x-min: '))
+            xmax = int(input('Enter x-max: '))
+            if xmin >= xmax:
+                print("Make sure that xmin is lower than xmax")
+            else:
+                graph(equation, xmin, xmax)
+            happy = 'no'
         else:
-            graph(equation, xmin, xmax)
-        happy = 'no'
-    else:
-        happy = 'yes'
+            happy = 'yes'
 
-# Ask user to estimate the value of the 1st root from the plot
-r1estimate = float(input('Estimate the x-value of the 1st root: '))
-code = parser.expr(equation).compile()
+    # Ask user to estimate the value of the 1st root from the plot
+    r1estimate = float(input('Estimate the x-value of the 1st root: '))
+    code = parser.expr(equation).compile()
 
-a = r1estimate - 1
-b = r1estimate + 1
-tol = 0.001
-roots = []
+    a = r1estimate - 1
+    b = r1estimate + 1
+    tol = 0.001
+    roots = []
 
-n = 1
-nmax = 500
-while nmax > n:
-    n = n+1
+    n = 1
+    while n < 500:
+        n = n+1
 
-    x = a
-    fa = eval(code)
-    if fa == 0:
-        print(x)
+        x = a
+        fa = eval(code)
+        if fa == 0:
+            print(x)
 
-    x = b
-    fb = eval(code)
-    if fb == 0:
-        print(x)
+        x = b
+        fb = eval(code)
+        if fb == 0:
+            print(x)
 
-    xmid = ((a+b)/2)
-    x = xmid
-    fxmid = eval(code)
-    if (fxmid < tol):
-        print("Trying x={0}....".format(x))
-        print("y={0}".format(fxmid))
-        print("x={0} is one of the roots!".format(x))
-        roots.append(x)
-        print("Roots = {0}".format(roots))
-        n = n+100000
-        break
-    elif fxmid*fa > 0.0:
-        b = xmid
-    elif fxmid*fb > 0.0:
-        a = xmid
+        xmid = ((a+b)/2)
+        x = xmid
+        fxmid = eval(code)
+        if fxmid < tol:
+            print("Trying x={0}....".format(x))
+            print("y={0}".format(fxmid))
+            print("x={0} is one of the roots!".format(x))
+            roots.append(x)
+            print("Roots = {0}".format(roots))
+            n = n+100000
+            break
+        elif fxmid*fa > 0.0:
+            b = xmid
+        elif fxmid*fb > 0.0:
+            a = xmid
+
+if __name__ == '__main__':
+    main()
