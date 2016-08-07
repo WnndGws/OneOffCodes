7cc4808e25dc8db1758a803833d8d3a1de869678
WG <ya@rp.com>
2016-08-07 10:24:25 +0800

    Made a start

--- lazy_teacher.py
+++ lazy_teacher.py
@@ -3,26 +3,28 @@
 '''Check list of files and determine which one to unzip'''
 
 import datetime as dt
+import os
 
 
 def main():
-    file_list = {'2016/08/06': 'filexx.zip',
-                 '2016/08/25': 'filexy.zip',
-                 '2016/08/26': 'filexz.zip'}
+    file_list = {'2016/08/07': 'filexx.z',
+                 '2016/08/25': 'filexy.z',
+                 '2016/08/26': 'filexz.z'}
 
     current_date = dt.date.today().strftime('%Y/%m/%d')
 
     try:
         target_file = file_list[current_date]
+        os.system('7z x {0} {1}'.format(target_file, target_file.split(".", 1)[0])
     except KeyError:
         print('No matching files for today')
         return None
 
-    # FTP into server and unzip file
+    # FTP into server and unzip file (EASIER OPTION)
     #           OR
     # Unzip file locally and upload onto FTP server
     # whatever one is easier
 
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
