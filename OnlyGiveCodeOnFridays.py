7a598ba6975c20411da42f2e66618e50f71d9cc6
ammgws <ammgws@users.noreply.github.com>
2016-10-20 00:15:32 +0900

    reformat code a little bit
    if salt is 'bla bla bla' then salt.encode() will encode the string into
    bytes, using the python3 default encoding (utf-8)
    
    or could just declare salt as b'bla bla bla' to acheive the same thing

--- OnlyGiveCodeOnFridays.py
+++ OnlyGiveCodeOnFridays.py
@@ -3,7 +3,7 @@ import datetime as dt
 import hashlib
 import re
 
-salt = '5618418165421843546546436546463546984' #just random gibberish I typed in
+salt = b'5618418165421843546546436546463546984'  # byte-string of random numbers
 
 now_date = dt.date.today()
 time_to_fri = dt.timedelta((4 - now_date.weekday()) % 7)
@@ -11,7 +11,7 @@ next_friday = now_date + time_to_fri
 next_code = next_friday.strftime('%m') + next_friday.strftime('%d')
 
 '''Im sure theres a more elegant way of doing the following, effectively what I'm trying to do is have a reproducable code, but that can't be guessed'''
-new_pw = hashlib.pbkdf2_hmac('sha256', next_code.encode(), salt.encode(), 100000)
+new_pw = hashlib.pbkdf2_hmac('sha256', next_code.encode(), salt, 100000)
 new_pw = binascii.hexlify(new_pw)
 new_pw = re.findall(r'\d+', str(new_pw))
 new_pw = "".join(new_pw)
