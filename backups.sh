80c2079eb372525e8f4f0bfd5abba93da3810d65
WG <wynandgouwswg@gmail.com>
2017-02-11 16:36:10 +0800

    Checking for different files, delete them and replace

--- backups.sh
+++ backups.sh
@@ -30,19 +30,29 @@ borg prune ~/wynZFS/Wynand/Backups/Antergos/ --prefix "{hostname}-" --keep-hourl
 
 # Check backups and alert if issues
 echo "Checking........"
-borg check ~/wynZFS/Wynand/Backups/GoogleDrive/ &>> ~/wynZFS/Wynand/Backups/tmp.txt
-borg check ~/wynZFS/Wynand/Backups/Antergos/ &>> ~/wynZFS/Wynand/Backups/tmp.txt
-if grep -Fq "Completed repository check, errors found" ~/wynZFS/Wynand/Backups/tmp.txt
+borg check ~/wynZFS/Wynand/Backups/GoogleDrive/ &>> ~/wynZFS/Wynand/Backups/.tmp.txt
+borg check ~/wynZFS/Wynand/Backups/Antergos/ &>> ~/wynZFS/Wynand/Backups/.tmp.txt
+if grep -Fq "Completed repository check, errors found" ~/wynZFS/Wynand/Backups/.tmp.txt
 then
     notify-send "Backup Error" "There was an error found in one of the Borg backups"
-    rm -rf ~/wynZFS/Wynand/Backups/tmp.txt
+    rm -rf ~/wynZFS/Wynand/Backups/.tmp.txt
 else
-    rm -rf ~/wynZFS/Wynand/Backups/tmp.txt
+    rm -rf ~/wynZFS/Wynand/Backups/.tmp.txt
+    
     # Only copy files to SDD and mega if no errors
     echo "Copying........."
+    
+    # Need to see if any files changed, and delete them from mega so that the new files can be uploaded
+    diff -qrN ~/wynZFS/Wynand/Backups /mnt/328E16488E16054F/AntergosBackups/Backups | cut -d \  -f 4 >~/wynZFS/Wynand/Backups/.tmp.txt
+    cat ~/wynZFS/Wynand/Backups/.tmp.txt | xargs -i rm {}
+    sed -i 's/\/mnt\/328E16488E16054F\/AntergosBackups\//\/Root\//g' ~/wynZFS/Wynand/Backups/.tmp.txt
+    cat ~/wynZFS/Wynand/Backups/.tmp.txt | xargs -i megarm -u ${mega_user} -p ${mega_password} {}
+    rm -rf ~/wynZFS/Wynand/Backups/.tmp.txt
+
     # Copy to Windows Drive
     rsync -rtuvc --progress --delete-delay ~/wynZFS/Wynand/Backups /mnt/328E16488E16054F/AntergosBackups
 
     #Upload to mega.nz
+    echo "Uploading......."
     nocorrect megacopy -u ${mega_user} -p ${mega_password} -r /Root/Backups -l  ~/wynZFS/Wynand/Backups
 fi
