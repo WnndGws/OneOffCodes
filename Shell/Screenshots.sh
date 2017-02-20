8ec0a063db7641c004a0a0db6533e918433becc1
wyntergos <wynandgouwswg@gmail.com>
2017-02-20 10:57:21 +0800

    Million Changes

---
+++ Shell/Screenshots.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+echo "Select area to share......"
+sleep 0.2
+scrot '/home/wynand/GoogleDrive/01_Personal/01_Personal/06_Images/Screenshots/%Y-%m-%d-%T.png' -q 50 -s
+sleep 3
+while [[ $(insync get_status) =~ "SYNCING" ]] { sleep 0.5 }
+sleep 1
+find /home/wynand/GoogleDrive/01_Personal/01_Personal/06_Images/Screenshots -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" " | sed 's/\.\//\/home\/wynand\/GoogleDrive\/01_Personal\/01_Personal\/06_Images\/Screenshots\//g' | xargs -i insync get_link {}
--- Shell/backups.sh
+++ Shell/backups.sh
@@ -12,24 +12,29 @@ export mega_user
 export mega_password
 export google_password
 
+# Monitor files for changes
+inotifywait -mr -e modify -e move -e create -e delete --format "%e %w%f" /home/wynand/wynZFS/Wynand/Backups -o ~/log
+
+# Backup my crontab
+crontab -l > ~/GoogleDrive/01_Personal/05_Software/Antergos/wyntergos_crontab
+
 #Create daily update of GoogleDrive
-borg create -p -C lz4 ~/wynZFS/Wynand/Backups/Antergos/::"{hostname}-{now:%Y%m%d-%H%M}" /home --exclude "*cache*" --exclude ~/Downloads --exclude ~/wynZFS --exclude "*.nohup*" --exclude "*steam*" --exclude "*Steam*"
+borg create -p -C lz4 /wynZFS/Wynand/Backups/Antergos/::"{hostname}-{now:%Y%m%d-%H%M}" /home --exclude "*cache*" --exclude ~/Downloads --exclude ~/wynZFS --exclude "*.nohup*" --exclude "*steam*" --exclude "*Steam*"
 
 # Backup Gmail
-expect ~/GoogleDrive/01_Personal/05_Software/Antergos/gmail_expect_script.exp ${google_password}
+# expect ~/GoogleDrive/01_Personal/05_Software/Antergos/gmail_expect_script.exp ${google_password}
 
 # Save packages and configurations
 expect ~/GoogleDrive/01_Personal/05_Software/Antergos/aconfmgr_expect_script.exp ${BORG_PASSPHRASE}
 
  #Prune Backups
 echo "Pruning........."
-borg prune ~/wynZFS/Wynand/Backups/GoogleDrive/ --prefix "{hostname}-" --keep-hourly=24 --keep-daily=14 --keep-weekly=8 --keep-monthly=12 --keep-yearly=10
-borg prune ~/wynZFS/Wynand/Backups/Antergos/ --prefix "{hostname}-" --keep-hourly=24 --keep-daily=14 --keep-weekly=8 --keep-monthly=12 --keep-yearly=10
+borg prune /wynZFS/Wynand/Backups/Antergos/ --prefix "{hostname}-" --keep-hourly=24 --keep-daily=14 --keep-weekly=8 --keep-monthly=12 --keep-yearly=10
 
 # Check backups and alert if issues
 echo "Checking........"
-borg check ~/wynZFS/Wynand/Backups/GoogleDrive/ &>> ~/wynZFS/Wynand/Backups/.tmp.txt
-borg check ~/wynZFS/Wynand/Backups/Antergos/ &>> ~/wynZFS/Wynand/Backups/.tmp.txt
+borg check /wynZFS/Wynand/Backups/Antergos/ &>> ~/wynZFS/Wynand/Backups/.tmp.txt
+
 if grep -Fq "Completed repository check, errors found" ~/wynZFS/Wynand/Backups/.tmp.txt
 then
     notify-send "Backup Error" "There was an error found in one of the Borg backups"
@@ -39,19 +44,19 @@ else
     
     # Only copy files to HDD and mega if no errors
    
-    echo "Finding duplicates..."
+    echo "Finding changed files..."
     # Need to see if any files changed, and delete them from mega so that the new files can be uploaded
-    diff -qrN ~/wynZFS/Wynand/Backups /mnt/328E16488E16054F/AntergosBackups/Backups | cut -d \  -f 4 >~/wynZFS/Wynand/Backups/.tmp.txt
+    diff -qrN /wynZFS/Wynand/Backups /run/media/wynand/Wyntergos_Backups/Backups | cut -d \  -f 4 >~/wynZFS/Wynand/Backups/.tmp.txt
     cat ~/wynZFS/Wynand/Backups/.tmp.txt | xargs -i rm -rf {}
-    sed -i 's/\/mnt\/328E16488E16054F\/AntergosBackups\//\/Root\//g' ~/wynZFS/Wynand/Backups/.tmp.txt
+    sed -i 's/\/run\/media\/wynand\/Wyntergos_Backups\/Backups\//\/Root\//g' ~/wynZFS/Wynand/Backups/.tmp.txt
     cat ~/wynZFS/Wynand/Backups/.tmp.txt | xargs -i megarm -u ${mega_user} -p ${mega_password} {}
     rm -rf ~/wynZFS/Wynand/Backups/.tmp.txt
 
     echo "Copying........."
-    # Copy to Windows Drive
-    cp -Lruv ~/wynZFS/Wynand/Backups /mnt/328E16488E16054F/AntergosBackups | mbuffer -P 75 -m 8M
+    # Copy to External Drive
+    cp -Lruv ~/wynZFS/Wynand/Backups /run/media/wynand/Wyntergos_Backups | mbuffer -P 75 -m 8M
 
     #Upload to mega.nz
     echo "Uploading......."
-#   nocorrect megacopy -u ${mega_user} -p ${mega_password} -r /Root/Backups -l  ~/wynZFS/Wynand/Backups
+#   nocorrect megacopy -u ${mega_user} -p ${mega_password} -r /Root/Backups -l  /wynZFS/Wynand/Backups
 fi
