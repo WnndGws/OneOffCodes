c481dec19a88fe4adb5a7e83d75f6ab831438be3
WG <wynand@gouws.com.au>
2017-08-15 14:13:27 +0800

    Ping test couldnt handle floating point math before
    So was just pinging constantly

--- Shell/pingtest.sh
+++ Shell/pingtest.sh
@@ -4,22 +4,26 @@
 connected_network=$(iwgetid | grep -o "\".*\"" | sed 's/\"//g')
 echo $connected_network
 sleep_time=1
-timeout 0.5 fping -c1 google.com
+timeout 1.0 fping -c1 google.com
 exit_code=$(echo $?)
 echo $exit_code
 
 while /bin/true; do
-    timeout 0.5 fping -c1 google.com
+    timeout 1.0 fping -c1 google.com
     exit_code=$(echo $?)
     if [[ "$exit_code" -eq 124 ]]; then
-        nmcli con up id $connected_network
-        sleep_time=1
-        sleep 10
+        timeout 1.0 fping -c1 google.com
+        exit_code=$(echo $?)
+        if [[ "$exit_code" -eq 124 ]]; then
+            nmcli con up id $connected_network
+            sleep_time=1
+            sleep 10
+        fi
     elif [[ "$sleep_time" -gt 600 ]]; then
         echo "Sleeping for 600"
         sleep 600
     else
-        sleep_time=$(expr $sleep_time \* 1.5)
+        sleep_time=$(echo "$sleep_time * 1.5" | bc)
         echo "Sleeping for $sleep_time"
         sleep $sleep_time
     fi
