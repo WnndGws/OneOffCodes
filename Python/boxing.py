f3e855a7f9ed4726ebf5aeed2e9ca97b33e3e7a8
WG <wynand@gouws.com.au>
2017-09-06 19:25:10 +0800

    Working on boxing scraping

--- Python/boxing.py
+++ Python/boxing.py
@@ -4,10 +4,15 @@ from bs4 import BeautifulSoup
 import urllib.request
 
 # Step 01-import list of names I care about from wiki
-champWikiURL = 'https://en.wikipedia.org/wiki/List_of_current_world_boxing_champions'
-page = urllib.request.urlopen(champWikiURL)
-soup = BeautifulSoup(page)
-champTables = soup.findAll("table", {"class" : "wikitable"})
+champURL = 'http://fightnews.com/rankings-2'
+page = urllib.request.urlopen(champURL)
+soup = BeautifulSoup(page, "lxml")
 
-print(champTables)
+# Find the names of each sanctioning body
+sanctioningBodies = [td.getText() for td in soup.findAll('tr', limit=4)[3].findAll('td')]
+# I am finding the first 4 table rows, then reading the 4th row, in that i am finding
+# the td of that row, and using list comprehension to print the contents, giving me
+# table headers
+
 ## https://github.com/rocheio/wiki-table-scrape/blob/master/wikitablescrape.py
+## http://savvastjortjoglou.com/nba-draft-part01-scraping.html
