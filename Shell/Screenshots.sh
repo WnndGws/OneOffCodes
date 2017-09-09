d5afa159ff85bc5decabc9b3ca49a907837e0b3b
WG <wynand@gouws.com.au>
2017-09-09 14:50:07 +0800

    Now open top on second screen so can monitor whats happening with hardware

--- Shell/Screenshots.sh
+++ Shell/Screenshots.sh
@@ -2,8 +2,8 @@
 
 echo "Select area to share......"
 sleep 0.1
-scrot '/home/wynand/GoogleDrive/01_Personal/01_Personal/06_Images/Screenshots/%Y-%m-%d-%T.png' -q 100 -a
-file_to_share=$(find /home/wynand/GoogleDrive/01_Personal/01_Personal/06_Images/Screenshots -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" " | sed 's/\.\//\/home\/wynand\/GoogleDrive\/01_Personal\/01_Personal\/06_Images\/Screenshots\//g')
+scrot '/home/wynand/GoogleDrive/01_Personal/01_Personal/05_Images/Screenshots/%Y-%m-%d-%T.png' -q 100 -a
+file_to_share=$(find /home/wynand/GoogleDrive/01_Personal/01_Personal/05_Images/Screenshots -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" " | sed 's/\.\//\/home\/wynand\/GoogleDrive\/01_Personal\/01_Personal\/05_Images\/Screenshots\//g')
 xclip -selection clipboard -t image/png -i $file_to_share
 secs=$((15))
 while [ $secs -gt 0 ]; do
--- Shell/preLands.sh
+++ Shell/preLands.sh
@@ -5,5 +5,6 @@ nmcli con down id USA\ -\ Los\ Angeles
 redshift -x
 kill $(pgrep redshift)
 nohup obs &
+tilix --maximize --geometry=1440x900+3840+180 -e 'zsh -c "tilix -a session-add-down -e radeontop; htop"'
 amixer sset 'Master' 85%
 nohup steam-native &
