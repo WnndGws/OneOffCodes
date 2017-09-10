4d15288ee8e32a595d18757b77bba18a446ba40e
WG <wynand@gouws.com.au>
2017-09-10 12:35:55 +0800

    Using nethogs as monitor as well

--- Shell/preLands.sh
+++ Shell/preLands.sh
@@ -4,7 +4,7 @@
 nmcli con down id USA\ -\ Los\ Angeles
 redshift -x
 kill $(pgrep redshift)
-nohup obs &
-tilix --maximize --geometry=1440x900+3840+180 -e 'zsh -c "tilix -a session-add-down -e radeontop; htop"'
+nohup obs --startreplaybuffer &
+tilix --maximize --geometry=1440x900+3840+180 -e 'zsh -c "tilix -a session-add-down -e radeontop; tilix -a session-add-right -e sudo nethogs; htop"'
 amixer sset 'Master' 85%
 nohup steam-native &
