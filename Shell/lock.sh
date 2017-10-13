3dbf0e03e6269716ec9488b19ae57628a663c9dd
WG <wynand@gouws.com.au>
2017-10-13 17:08:54 +0800

    If muted before locking, then leave muted, else toggle
    This way computer is always muted when locked

--- Shell/lock.sh
+++ Shell/lock.sh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 ## Locks and mutes PC
 
-amixer sset 'Master' 00% 2>&1 /dev/null
-# Mutes
+# Mutes and unmutes
+# muted status (yes = muted)
+active_sink=$(pacmd list-sinks | awk '/* index:/{print $3}')
+muteStatus=$(pacmd list-sinks | grep -A 15 'index: '$active_sink | grep 'muted' | awk '{print $2}')
+
+# Mute even if already muted, but dont change muteStatus as this will be used as our 'before'
+pactl set-sink-mute $active_sink 1
 
 scrot /tmp/screen.png
 # Screenshot
@@ -13,7 +18,14 @@ convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f ~/.dotfiles/i3/.config/i3/lock.png ]] && convert /tmp/screen.png ~/.dotfiles/i3/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
 # Adds lock image over the blurry screenshot
 
-i3lock --no-unlock-indicator --ignore-empty-password --image=/tmp/screen.png --nofork && exit
+i3lock --no-unlock-indicator --ignore-empty-password --image=/tmp/screen.png --nofork &&\
+if [ $muteStatus = "no" ]
+then
+    pactl set-sink-mute $active_sink 0
+else
+    exit
+fi
+
 trash-empty
 
 #xset dpms force off
