348e879b2eea5c52c94296950d15ec59fb706130
WG <wynand@gouws.com.au>
2017-10-13 10:43:34 +0800

    Use pixelated images as lock instead of black

--- Shell/lock.sh
+++ Shell/lock.sh
@@ -4,7 +4,12 @@
 amixer sset 'Master' 00%
 # Mutes
 
-i3lock -c 000000
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 100% /temp/screen.png
+[[ -f ~/.dotfiles/i3/.config/i3/lock.png ]] && convert /tmp/screen.png ~/.dotfiles/i3/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+
+
+i3lock --no-unlock-indicator --ignore-empty-password --image=/tmp/screen.png
 trash-empty
 xset dpms force off
 # Turns off screen
