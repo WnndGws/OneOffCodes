60d22ee382928ae20c887e86e2f2567772293195
WG <wynand@gouws.com.au>
2017-10-13 11:00:05 +0800

    Need to scale down, then back 1000% to get to original size

--- Shell/lock.sh
+++ Shell/lock.sh
@@ -5,11 +5,10 @@ amixer sset 'Master' 00%
 # Mutes
 
 scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 100% /temp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f ~/.dotfiles/i3/.config/i3/lock.png ]] && convert /tmp/screen.png ~/.dotfiles/i3/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
-
 
 i3lock --no-unlock-indicator --ignore-empty-password --image=/tmp/screen.png
 trash-empty
-xset dpms force off
+#xset dpms force off
 # Turns off screen
