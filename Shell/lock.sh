e3d0848ff7a8a24474c602d2c7b9edb3a65bc162
WG <wynand@gouws.com.au>
2017-10-13 11:39:44 +0800

    Added comments

--- Shell/lock.sh
+++ Shell/lock.sh
@@ -5,10 +5,16 @@ amixer sset 'Master' 00% 2>&1 /dev/null
 # Mutes
 
 scrot /tmp/screen.png
+# Screenshot
+
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+# Shrinks down, then resizes up so screenshot is blurry
+
 [[ -f ~/.dotfiles/i3/.config/i3/lock.png ]] && convert /tmp/screen.png ~/.dotfiles/i3/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+# Adds lock image over the blurry screenshot
 
 i3lock --no-unlock-indicator --ignore-empty-password --image=/tmp/screen.png
 trash-empty
+
 #xset dpms force off
 # Turns off screen
