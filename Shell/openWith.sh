9d24427676aff7835674b24496b02b4b3abf773e
WG <wynand@gouws.com.au>
2017-10-24 17:10:26 +0800

    Openwith

---
+++ Shell/openWith.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+## A Simple script to open the correct files with the correct program
+
+
+# A function containing a case list of options
+open() {
+    case "$1" in
+        *youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi) mpv --x11-name=mpv_youtube --really-quiet --autofit=30% --geometry=-10-15 --ytdl --ytdl-format="mp4[height<=?720]" "$1" &! ;;
+        *.png|*.jpeg|*.gif*|*.jpg) feh -. "$1";;  # feh -. = opens to fit window.
+        *) google-chrome-stable "$1";  # For everything else.;
+    esac
+}
+
+# Now a for loop to iterate the list of options,
+for url; do
+    open "$url"
+done
