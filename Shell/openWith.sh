5f8ada41f9efe046de09a65f5c050df490e4fb90
WG <wynand@gouws.com.au>
2017-10-27 11:24:32 +0800

    Moved starting position to line up better with my shells

--- Shell/openWith.sh
+++ Shell/openWith.sh
@@ -5,7 +5,7 @@
 # A function containing a case list of options
 open() {
     case "$1" in
-        *youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi) mpv --x11-name=mpv_youtube --really-quiet --autofit=30% --geometry=-10-15 --ytdl --ytdl-format="mp4[height<=?720]" "$1" &! ;;
+        *youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi) mpv --x11-name=mpv_youtube --really-quiet --autofit=30% --geometry=-15-55 --ytdl --ytdl-format="mp4[height<=?720]" "$1" &! ;;
         *.png|*.jpeg|*.gif*|*.jpg) feh -. "$1";;  # feh -. = opens to fit window.
         *) google-chrome-stable "$1";  # For everything else.;
     esac
