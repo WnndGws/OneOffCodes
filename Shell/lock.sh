2f9a3eebd34b16b86f90571c907eba51f7f39a05
WG <wynand@gouws.com.au>
2017-10-27 11:23:29 +0800

    Use ffmpeg instead of imagemagic
    MUCH faster.
    Also moved all mute stuff to after lock, so can lock asap

--- Shell/lock.sh
+++ Shell/lock.sh
@@ -1,6 +1,23 @@
 #!/bin/zsh
 ## Locks and mutes PC
 
+# Find resolution of screen(s)
+resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
+
+# Location of lock image
+lock_image="$HOME/.dotfiles/i3/.config/i3/lock.png"
+
+# Set filters to apply to the image
+filters='noise=alls=10,scale=iw*.05:-1,scale=iw*20:-1:flags=neighbor'
+
+# Output file location
+output_loc="/tmp/screen.png"
+
+# Take screenshot and apply filters, and overlay lock_image
+ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i $DISPLAY -i $lock_image -vframes 1 -vf "$filters" -filter_complex 'overlay' $output_loc
+
+i3lock --no-unlock-indicator --ignore-empty-password --image=$output_loc --nofork &&\
+
 # Mutes and unmutes
 # muted status (yes = muted)
 active_sink=$(pacmd list-sinks | awk '/* index:/{print $3}')
@@ -9,17 +26,6 @@ muteStatus=$(pacmd list-sinks | grep -A 15 'index: '$active_sink | grep 'muted' 
 # Mute even if already muted, but dont change muteStatus as this will be used as our 'before'
 pactl set-sink-mute $active_sink 1
 
-scrot /tmp/screen.png
-# Screenshot
-
-convert /tmp/screen.png -scale 5% -scale 2000% /tmp/screen.png
-convert /tmp/screen.png -paint 1 /tmp/screen.png
-# Shrinks down, then resizes up so screenshot is blurry
-
-[[ -f $HOME/.dotfiles/i3/.config/i3/lock.png ]] && convert /tmp/screen.png $HOME/.dotfiles/i3/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
-# Adds lock image over the blurry screenshot
-
-i3lock --no-unlock-indicator --ignore-empty-password --image=/tmp/screen.png --nofork &&\
 if [ $muteStatus = "no" ]
 then
     pactl set-sink-mute $active_sink 0
