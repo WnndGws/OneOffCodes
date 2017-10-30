307ef43a7815df1cb367bbf31acfd16de333f049
WG <wynand@gouws.com.au>
2017-10-30 09:51:46 +0800

    No longer floating it

--- Shell/openWith.sh
+++ Shell/openWith.sh
@@ -5,7 +5,7 @@
 open() {
     case "$1" in
         *youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif) mpv --x11-name=mpv_youtube --really-quiet --autofit=30% --geometry=-15-55 --ytdl --ytdl-format="mp4[height<=?720]" "$1" &! ;;
-        *.png|*.jpeg|*.jpg) feh -. --geometry 600x600 "$1";;  # feh -. = opens to fit window.
+        *.png|*.jpeg|*.jpg) feh -. "$1";;  # feh -. = opens to fit window.
         *) google-chrome-stable "$1";  # For everything else.;
     esac
 }
