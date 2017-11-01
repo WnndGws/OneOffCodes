efa97d1293d1ec8772432a9fcf8c09fb09950279
WG <wynand@gouws.com.au>
2017-11-01 10:27:57 +0800

    i3 way

--- Shell/openWith.sh
+++ Shell/openWith.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 ## A Simple script to open the correct files with the correct program
+## Same as openwith, but doesnt set mpvf lag to make it seethrough and float
 
 # A function containing a case list of options
 open() {
     case "$1" in
-        *youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif) mpv --x11-name=mpv_youtube --really-quiet --autofit=30% --geometry=-15-55 --ytdl --ytdl-format="mp4[height<=?720]" "$1" &! ;;
+        *youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif) mpv --really-quiet --ytdl --ytdl-format="mp4[height<=?720]" "$1" &! ;;
         *.png|*.jpeg|*.jpg) feh -. "$1";;  # feh -. = opens to fit window.
         *) google-chrome-stable "$1";  # For everything else.;
     esac
