80f60a9aba4a62e1ee7299daefd62014fb6e914e
WG <wynand@gouws.com.au>
2017-11-10 09:32:40 +0800

    Long options

--- Shell/openWith.sh
+++ Shell/openWith.sh
@@ -5,8 +5,8 @@
 # A function containing a case list of options
 open() {
     case "$1" in
-        *v.redd.it*|*imgtc*|*youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif) mpv --really-quiet "$1" &! ;;
-        *.png|*.jpeg|*.jpg) feh -. "$1";;  # feh -. = opens to fit window.
+        *v.redd.it*|*imgtc*|*youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif) mpv --really-quiet --loop "$1" &! ;;
+        *.png*|*.jpeg*|*.jpg*) feh --scale-down "$1";;  # feh -. = opens to fit window.
         *) google-chrome-stable "$1";  # For everything else.;
     esac
 }
