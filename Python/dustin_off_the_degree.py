9e4a48b9a780eecf96db8c6c059aff57657c99f3
WG <wynand@gouws.com.au>
2017-12-05 10:39:46 +0800

    Download mp3 too

--- Python/dustin_off_the_degree.py
+++ Python/dustin_off_the_degree.py
@@ -8,8 +8,17 @@ from bs4 import BeautifulSoup
 
 ep_dict = {}
 
+def download_mp3(url):
+    local_filename = url.split('/')[-1]
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+    return local_filename
+
 def itterate_through():
-    for n in range(1, 300):
+    for n in range(130, 250):
         url = f'http://atheistnomads.com/{n}'
         print(url)
         req = requests.get(url)
@@ -18,7 +27,9 @@ def itterate_through():
             para = soup.find_all('p')
             for words in para:
                 if re.match('.*(dustin).*(degree).*', words.text, re.IGNORECASE) is not None:
-                    ep_dict[n] = words.text
+                    ep_dict[n] = f'{words.text}\n'
+                    url = f'http://traffic.libsyn.com/forcedn/atheistnomads/atheist_nomads_{n}.mp3'
+                    download_mp3(url)
     with open('dustin_degree.txt', 'w') as file:
         file.write(json.dumps(ep_dict))
 
