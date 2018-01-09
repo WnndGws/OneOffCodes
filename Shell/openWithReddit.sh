258a0de59047d2802e45f21cbad90c7769cde217
WG <wynand@gouws.com.au>
2018-01-09 20:53:44 +0800

    Open new mpv instead of umpv

---
+++ Shell/openWithReddit.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+## A Simple script to open the correct files with the correct program
+## Same as openwith, but doesnt set mpvf lag to make it seethrough and float
+
+# A function containing a case list of options
+open() {
+    case "$1" in
+        *streamable*|*gfycat*|*v.redd.it*|*imgtc*|*youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif) mpv "$1" &! python ~/Git/OneOffCodes/Python/small_mpv.py &! ;;
+        *imgur*|*.png*|*.jpeg*|*.jpg*) feh --scale-down "$1";;  # feh -. = opens to fit window.
+        *) python $HOME/Git/OneOffCodes/Python/paragraph_scraper.py --url "$1" && urxvtc -e speedread -w 375 /tmp/para.txt
+        #*) google-chrome-stable "$1";  # For everything else.;
+    esac
+}
+
+# Now a for loop to iterate the list of options,
+for url; do
+    echo "$url" | xclip -selection clipboard
+    open "$url"
+done
