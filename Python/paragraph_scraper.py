a77bb4f95d5ca02cef02fa64ce526cd031d5c51a
WG <wynand@gouws.com.au>
2018-01-26 10:40:30 +0800

    Trying to add image scraper
    Not working great

--- Python/paragraph_scraper.py
+++ Python/paragraph_scraper.py
@@ -15,5 +15,22 @@ def print_paragraph_text(url):
         f = open('/tmp/para.txt', 'a')
         f.write(f'\n\n {words.text}')
 
+@click.command()
+@click.option('--url')
+def save_images(url):
+    file_number = 1
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, "html.parser")
+    for img in soup.find_all('img'):
+        img = (img['src'])
+        try:
+            req = requests.get(img, allow_redirects=True)
+            print(f'/tmp/image{file_number}')
+            open(f'/tmp/image{file_number}.png', 'wb').write(r.content)
+            file_number += 1
+        except:
+            pass
+
 if __name__ == '__main__':
     print_paragraph_text()
+    #save_images()
