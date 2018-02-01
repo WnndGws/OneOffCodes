8664b18e36fdeb78658c9979e06d63a41a26158d
Jason <jason@kayoway.com>
2018-02-01 23:36:59 +0900

    Pythonify code

--- Python/paragraph_scraper.py
+++ Python/paragraph_scraper.py
@@ -17,21 +17,15 @@ def print_paragraph_text(url):
 @click.command()
 @click.option('--url')
 def save_images(url):
-    file_number = 1
     s = requests.Session()
     req = s.get(url)
     soup = BeautifulSoup(req.content, "html.parser")
-    for img in soup.find_all('img'):
+    for file_number, img in enumerate(soup.find_all('img'), start=1):
         img = (img['src'])
-        try:
-            req = s.get(img, allow_redirects=True)
-            print(f'/tmp/image{file_number}')
-            with open(f'/tmp/image{file_number}.png', 'wb') as f:
-                f.write(req.content)
-            file_number += 1
-        except:
-            pass
+        req = s.get(img, allow_redirects=True)
+        with open(f'/tmp/image{file_number}.png', 'wb') as f:
+            f.write(req.content)
 
 if __name__ == '__main__':
-    print_paragraph_text()
-    #save_images()
+    #print_paragraph_text()
+    save_images()
