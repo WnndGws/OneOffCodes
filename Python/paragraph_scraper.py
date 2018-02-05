00ce8b80165d4ac5089fb982b6eb973089551e7d
WG <wynand@gouws.com.au>
2018-02-06 07:28:59 +0800

    Need to copy click setup from wallpapermaker

--- Python/paragraph_scraper.py
+++ Python/paragraph_scraper.py
@@ -2,29 +2,37 @@
 ## Takes the arguement of a url and outputs the paragraph text from that page to a textfile
 
 import click
+import re
 import requests
 from bs4 import BeautifulSoup
 
-@click.command()
-@click.option('--url')
-def print_paragraph_text(url):
-    req = requests.get(url)
+@click.command(
+    context_settings=dict(ignore_unknown_options=True,
+                          allow_extra_args=True,
+                          resilient_parsing=True))
+@click.option('--url_para')
+def print_paragraph_text(url_para):
+    req = requests.get(url_para)
     soup = BeautifulSoup(req.content, "html.parser")
     with open('/tmp/para.txt', 'a') as f:
-        for words in soup.find_all('p'):
+        for words in soup.findAll('p'):
             f.write(f'\n\n {words.text}')
 
-@click.command()
-@click.option('--url')
-def save_images(url):
+@click.command(
+    context_settings=dict(ignore_unknown_options=True,
+                          allow_extra_args=True,
+                          resilient_parsing=True))
+@click.option('--url_image')
+def save_images(url_image):
     s = requests.Session()
-    req = s.get(url)
+    req = s.get(url_image)
     soup = BeautifulSoup(req.content, "html.parser")
-    for file_number, img in enumerate(soup.find_all('img'), start=1):
+    for file_number, img in enumerate(soup.findAll('img'), start=1):
         img = (img['src'])
-        req = s.get(img, allow_redirects=True)
-        with open(f'/tmp/image{file_number}.png', 'wb') as f:
-            f.write(req.content)
+        if re.match(r'(http).*', img) is not None:
+            req = s.get(img, allow_redirects=True)
+            with open(f'/tmp/image{file_number}.png', 'wb') as f:
+                f.write(req.content)
 
 if __name__ == '__main__':
     print_paragraph_text()
