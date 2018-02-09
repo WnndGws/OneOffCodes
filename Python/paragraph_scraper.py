a82d19b6827222149756624cf3d427e372840cee
WG <wynand@gouws.com.au>
2018-02-09 09:04:57 +0800

    Simplify code

--- Python/paragraph_scraper.py
+++ Python/paragraph_scraper.py
@@ -6,27 +6,15 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-@click.command(
-    context_settings=dict(ignore_unknown_options=True,
-                          allow_extra_args=True,
-                          resilient_parsing=True))
-@click.option('--url_para')
-def print_paragraph_text(url_para):
-    req = requests.get(url_para)
+@click.option('--url')
+def scrape_content(url):
+    s = requests.Session()
+    req = s.get(url)
     soup = BeautifulSoup(req.content, "html.parser")
     with open('/tmp/para.txt', 'a') as f:
         for words in soup.findAll('p'):
             f.write(f'\n\n {words.text}')
 
-@click.command(
-    context_settings=dict(ignore_unknown_options=True,
-                          allow_extra_args=True,
-                          resilient_parsing=True))
-@click.option('--url_image')
-def save_images(url_image):
-    s = requests.Session()
-    req = s.get(url_image)
-    soup = BeautifulSoup(req.content, "html.parser")
     for file_number, img in enumerate(soup.findAll('img'), start=1):
         img = (img['src'])
         if re.match(r'(http).*', img) is not None:
@@ -35,5 +23,4 @@ def save_images(url_image):
                 f.write(req.content)
 
 if __name__ == '__main__':
-    print_paragraph_text()
-    save_images()
+    scrape_content()
