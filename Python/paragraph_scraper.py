4738c0867ab78d3f12b6782f25fdf12faa400d32
WG <wynand@gouws.com.au>
2018-02-09 14:44:59 +0800

    No more click

--- Python/paragraph_scraper.py
+++ Python/paragraph_scraper.py
@@ -1,12 +1,21 @@
 #!/bin/python
 ## Takes the arguement of a url and outputs the paragraph text from that page to a textfile
 
-import click
 import re
 import requests
+import sys
+
 from bs4 import BeautifulSoup
 
-@click.option('--url')
+## Collect command-line arguements from input
+def getopts(argv):
+    opts = {}  # Empty dictionary to store key-value pairs.
+    while argv:  # While there are arguments left to parse...
+        if argv[0][0] == '-':  # Found a "-name value" pair.
+            opts[argv[0]] = argv[1]  # Add key and value to the dictionary.
+        argv = argv[1:]  # Reduce the argument list by copying it starting from index 1.
+    return opts
+
 def scrape_content(url):
     s = requests.Session()
     req = s.get(url)
@@ -23,4 +32,8 @@ def scrape_content(url):
                 f.write(req.content)
 
 if __name__ == '__main__':
-    scrape_content()
+    from sys import argv
+    myargs = getopts(argv)
+    if '--url' in myargs:
+        url = myargs['--url']
+    scrape_content(url)
