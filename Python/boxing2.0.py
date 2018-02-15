8e6138c69a731e83ea796f1ce4bb8674535b532f
WG <wynand@gouws.com.au>
2018-02-15 09:19:47 +0800

    Poll google calendar to find whats coming up
    And compare with list of top boxers

--- Python/boxing2.0.py
+++ Python/boxing2.0.py
@@ -1,6 +1,16 @@
 ## Script made to check when there is a middleweight+ title fight, or when 'the ring' top 10 fight
 
+from __future__ import print_function
+from apiclient import discovery
 from bs4 import BeautifulSoup
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import argparse
+import datetime
+import httplib2
+import os
 import re
 import urllib.request
 
@@ -25,4 +35,57 @@ for table_number in range(2,6):
                         unique_boxers.append(boxer_name[0])
 
 unique_boxers.sort()
-print(unique_boxers)
+
+flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'boxing2.0_client_id.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets('~/.credentials/client_secrets_boxing2.0.json', scope='https://www.googleapis.com/auth/calendar')
+        flow.user_agent = 'Boxing2.0'
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+credentials = get_credentials()
+http = credentials.authorize(httplib2.Http())
+service = discovery.build('calendar', 'v3', http=http)
+
+now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+nowMonth = datetime.datetime.utcnow() + datetime.timedelta(days=31)
+nowMonth = nowMonth.isoformat() + 'Z'
+
+eventsResult = service.events().list(
+    calendarId='1krp7iu4q65i0qt6eagdjj5ucs@group.calendar.google.com', timeMin=now, timeMax=nowMonth, singleEvents=True,
+    orderBy='startTime').execute()
+events = eventsResult.get('items', [])
+
+nextMonthEvents = []
+for event in events:
+    eventTitle = event['summary']
+    boxer_one = re.findall(r'.+?(?= vs )', eventTitle)
+    boxer_two = re.findall(r'(?<=vs )(.*)(?= -)', eventTitle)
+    nextMonthEvents.append(boxer_one[0])
+    nextMonthEvents.append(boxer_two[0])
+
+boxer_i_care_about = set(unique_boxers).intersection(nextMonthEvents)
+print(boxer_i_care_about)
