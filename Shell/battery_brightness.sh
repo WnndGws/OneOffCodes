98aec6a05bfa75ac08574ec94b1a1d9d79245fc1
WG <wynand@gouws.com.au>
2018-02-19 17:24:07 +0800

    No need for two condition tests

--- Shell/battery_brightness.sh
+++ Shell/battery_brightness.sh
@@ -3,8 +3,6 @@
 
 battery_percentage=$(acpi --battery | rg -o '[0-9]{2}%')
 
-if [[  $battery_percentage == "00%" ]]; then
-    break
-else
+if [[  $battery_percentage != "00%" ]]; then
     brightnessctl s $battery_percentage
 fi
