7aa70518e3ea04a2112aec985363207f860ed70a
WG <wynand@gouws.com.au>
2018-03-19 11:06:31 +0800

    Firefox

--- Shell/openWith_noImages.sh
+++ Shell/openWith_noImages.sh
@@ -5,10 +5,10 @@
 # A function containing a case list of options
 open() {
     case "$1" in
-        *streamable*|*gfycat*|*v.redd.it*|*imgtc*|*youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif) $HOME/Git/OneOffCodes/Python/umpv "$1" &! ;;
+        *stream*|*gfycat*|*v.redd.it*|*imgtc*|*youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif) $HOME/Git/OneOffCodes/Python/umpv "$1" &! ;;
         *imgur*|*.png*|*.jpeg*|*.jpg*) feh --scale-down "$1";;  # feh -. = opens to fit window.
-        *) rm -f /tmp/image* ; rm -f /tmp/para.txt ; python $HOME/Git/OneOffCodes/Python/paragraph_scraper.py --url "$1" > /dev/null 2>&1 ; urxvtc -geometry 50x2 -e speedread -w 375 /tmp/para.txt
-        #*) google-chrome-stable "$1";  # For everything else.;
+        *) rm -f /tmp/image* ; rm -f /tmp/para.txt ; python $HOME/Git/OneOffCodes/Python/paragraph_scraper/paragraph_scraper.py --url "$1" > /dev/null 2>&1 ; urxvtc -geometry 50x2 -e speedread -w 375 /tmp/para.txt
+        #*) firefox "$1";  # For everything else.;
     esac
 }
 
