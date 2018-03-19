9434bf2b30413f7063624ec3ab25ad762e974c7b
WG <wynand@gouws.com.au>
2018-03-19 11:06:17 +0800

    Firegfox

--- Shell/openWithReddit.sh
+++ Shell/openWithReddit.sh
@@ -5,10 +5,25 @@
 # A function containing a case list of options
 open() {
     case "$1" in
-        *streamable*|*gfycat*|*v.redd.it*|*imgtc*|*youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif) mpv "$1" &! ;;
+        *stream*|*gfycat*|*v.redd.it*|*imgtc*|*youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif|*vimeo|*vimeo.com*) mpv "$1" &! ;;
         *imgur*|*.png*|*.jpeg*|*.jpg*) feh --scale-down "$1";;  # feh -. = opens to fit window.
-        *) google-chrome-stable "$1";  # For everything else.;
+        *) firefox "$1";  # For everything else.;
     esac
+}
+
+others(){
+rm -f /tmp/image*
+rm -f /tmp/para.txt 
+python $HOME/Git/OneOffCodes/Python/paragraph_scraper/paragraph_scraper.py --url "$1" > /dev/null 2>&1
+if [[  $(wc -l /tmp/para.txt | awk '{print $1}') > 0  ]]
+then
+    urxvtc -geometry 50x2 -e speedread -w 375 /tmp/para.txt
+    python $HOME/Git/OneOffCodes/Python/image_scraper/image_scraper.py --url "$1" > /dev/null 2>&1
+    find /tmp/image* -print0 | xargs -0 identify -format "%w %f\n" | awk '$1<200' | xargs -0 | cut -d' ' -f2 | xargs -I{} rm /tmp/{}
+    feh --scale-down /tmp/image*
+else
+    firefox "$1"
+fi
 }
 
 # Now a for loop to iterate the list of options,
