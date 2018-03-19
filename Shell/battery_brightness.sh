57ce18c64329f1be81e80da28bc5c4d36d9c54d3
WG <wynand@gouws.com.au>
2018-03-19 11:05:31 +0800

    100% when charging

--- Shell/battery_brightness.sh
+++ Shell/battery_brightness.sh
@@ -2,7 +2,12 @@
 ## Sets the display brightness to match battery level
 
 battery_percentage=$(acpi --battery | rg -o '[0-9]{2}%')
+battery_state=$(acpi --battery | awk '{print $3}')
 
-if [[  $battery_percentage != "00%" ]]; then
-    brightnessctl s $battery_percentage
+if [[ $battery_state == "Charging," ]]; then
+    brightnessctl s 100%
+else
+    if [[  $battery_percentage != "00%" ]]; then
+        brightnessctl s $battery_percentage
+    fi
 fi
