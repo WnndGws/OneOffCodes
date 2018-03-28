5730755cd04c1f477913a78aeb75e5a07d149227
WG <wynand@gouws.com.au>
2018-03-28 11:45:18 +0800

    Commit files one at a time

--- Shell/gitPushAll.sh
+++ Shell/gitPushAll.sh
@@ -9,18 +9,19 @@ declare -a repos=(\
 "$HOME/Git/DS18B20_email_alert"
 )
 
+ssh-add
 for i in "${repos[@]}"; do
     cd "$i"
     git pull
 
     for file in $(git ls-files --others --exclude-standard); do
         git add $file
-        git commit -vS
+        git commit -vS $file
     done
 
     for file in $(git diff --name-only)
         git add -p $file
-        git commit -vS
+        git commit -vS $file
     done
 
 git push
