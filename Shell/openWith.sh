60e28299d238bbac2789f7fbe1d1b7d61ee8c682
WG <wynand@gouws.com.au>
2018-04-03 18:34:06 +0800

    Download then open as a way to deal with albums

--- Shell/openWith.sh
+++ Shell/openWith.sh
@@ -6,7 +6,7 @@
 open() {
     case "$1" in
         *stream*|*gfycat*|*v.redd.it*|*imgtc*|*youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif|*vimeo|*vimeo.com*) $HOME/Git/OneOffCodes/Python/umpv "$1" &! ;;
-        *imgur*|*.png*|*.jpeg*|*.jpg*) feh --scale-down "$1";;  # feh -. = opens to fit window.
+        *imgur*|*.png*|*.jpeg*|*.jpg*) gallery-dl --dest /tmp "$1" ; feh --scale-down /tmp/imgur && rm -rf /tmp/imgur;;  # feh -. = opens to fit window.
         *) others "$1"
         #*) firefox "$1";  # For everything else.;
     esac
