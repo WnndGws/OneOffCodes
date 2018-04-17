d46e02b5f76b2700483b8333ba7b4b1e9c3decea
WG <wynand@gouws.com.au>
2018-04-17 10:29:27 +0800

    Yes/no script

---
+++ Shell/dmenu_yn_prompt.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+# A dmenu binary prompt script.
+# Gives a dmenu prompt labeled with $1 to perform command $2.
+# For example:
+# `./prompt "Do you want to shutdown?" "shutdown -h now"`
+
+[[ $(echo -e "No\nYes" | dmenu -i -p "$1" -h 40 -fn "Sauce Code Pro Nerd Font Complete Mono:pixelsize=12;0") \
+== "Yes" ]] && zsh -c $2 
+
