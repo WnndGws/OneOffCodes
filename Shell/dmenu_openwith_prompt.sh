8553430af7ee55add60c535d44d35f8c5b211a84
WG <wynand@gouws.com.au>
2018-04-19 09:32:12 +0800

    Change text size

--- Shell/dmenu_openwith_prompt.sh
+++ Shell/dmenu_openwith_prompt.sh
@@ -11,7 +11,7 @@ visual="$1"
 fi
 echo $visual
 
-x=$(echo -e "mpv\nmpv (loop)\numpv\nfeh\nfirefox\nlink_handler" | dmenu -h 40 -fn "Sauce Code Pro Nerd Font Complete Mono:pixelsize=12;0" -i -p "How should I open '$visual'?")
+x=$(echo -e "mpv\nmpv (loop)\numpv\nfeh\nfirefox\nlink_handler\nlink_handler (no summary)" | dmenu -h 40 -fn "Sauce Code Pro Nerd Font Complete Mono:pixelsize=14;0" -i -p "How should I open '$visual'?")
 case "$x" in
 	mpv) mpv -quiet "$1" 2&>/dev/null & disown ;;
 	"mpv (loop)") zsh -c mpv -quiet --loop "$1" 2&>/dev/null & disown ;;
@@ -19,4 +19,5 @@ case "$x" in
 	firefox) firefox "$1" 2&>/dev/null & disown ;;
 	feh) rm -rf /tmp/images/* ; python $HOME/Git/OneOffCodes/Python/image_scraper/image_scraper.py --url "$1" ; feh --scale-down --recursive /tmp/images & disown;;
 	link_handler) urxvtc -hold -e $BROWSER "$1";;
+	"link_handler (no summary)") urxvtc -hold -e ~/Git/OneOffCodes/Shell/openWith_NoSummary.sh "$1";;
 esac
