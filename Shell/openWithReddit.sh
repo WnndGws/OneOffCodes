58cdfa83ba61b749a1994bb099ee90f29d2a8f9a
WG <wynand@gouws.com.au>
2018-04-19 09:32:46 +0800

    Reddit to match normal open

--- Shell/openWithReddit.sh
+++ Shell/openWithReddit.sh
@@ -5,22 +5,31 @@
 # A function containing a case list of options
 open() {
     case "$1" in
-        *stream*|*gfycat*|*v.redd.it*|*imgtc*|*youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif*|*vimeo*) mpv "$1" &! ;;
-        *imgur*|*.png|*.jpeg|*.jpg) rm -rf /tmp/imgur ; gallery-dl --dest /tmp/imgur "$1" ; feh --scale-down --recursive /tmp/imgur;;  # feh -. = opens to fit window.
-        *) firefox "$1";  # For everything else.;
+        *stream*|*gfycat*|*v.redd.it*|*imgtc*|*youtube.com*|*youtu.be*|*vodlocker.com*|*.webm*|*.mp4*|*.avi|*.gif*|*vimeo|*vimeo.com*) mpv --loop "$1" &! ;;
+        *imgur*|*.png|*.jpeg|*.jpg) rm -rf /tmp/images/* ; gallery-dl --dest /tmp/images "$1" ; feh --scale-down --recursive /tmp/images;;  # feh -. = opens to fit window.
+        *) others "$1"
+        #*) firefox "$1";  # For everything else.;
     esac
 }
 
 others(){
-rm -f /tmp/image*
-rm -f /tmp/para.txt 
+clear
+rm -rf /tmp/images/* > /dev/null 2>&1
+rm -f /tmp/para.txt > /dev/null 2>&1
+rm -f /tmp/para_summarise.txt > /dev/null 2>&1
 python $HOME/Git/OneOffCodes/Python/paragraph_scraper/paragraph_scraper.py --url "$1" > /dev/null 2>&1
-if [[  $(wc -l /tmp/para.txt | awk '{print $1}') > 0  ]]
+python $HOME/Git/OneOffCodes/Python/paragraph_scraper/article_summarise.py
+if [[  $(wc -l /tmp/para.txt | awk '{print $1}') > 10  ]]
 then
-    speedread -w 325 /tmp/para_summary.txt
-    python $HOME/Git/OneOffCodes/Python/image_scraper/image_scraper.py --url "$1" > /dev/null 2>&1
-    find /tmp/image* -print0 | xargs -0 identify -format "%w %f\n" | awk '$1<200' | xargs -0 | cut -d' ' -f2 | xargs -I{} rm /tmp/{}
-    feh --scale-down /tmp/image*
+    cat /tmp/para_summarise.txt | wc -l
+    cat /tmp/para_summarise.txt
+    #speedread -w 325 /tmp/para_summarise.txt
+    mkdir /tmp/images > /dev/null 2>&1
+    gallery-dl --dest /tmp/images "$1" > /dev/null 2>&1
+    #python $HOME/Git/OneOffCodes/Python/image_scraper/image_scraper.py --url "$1" > /dev/null 2>&1
+    feh --scale-down --recursive /tmp/images &
+    mpv --ytdl "$1" > /dev/null 2>&1
+    sleep 14400
 else
     firefox "$1"
 fi
