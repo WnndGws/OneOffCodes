4b880d6d6b8a5912a1bbc35cc56c5fd033d74c9e
WG <wynand@gouws.com.au>
2018-04-26 11:40:20 +0800

    Manually set screen

--- Shell/battery_brightness.sh
+++ Shell/battery_brightness.sh
@@ -5,9 +5,9 @@ battery_percentage=$(acpi --battery | rg -o '[0-9]{2}%')
 battery_state=$(acpi --battery | awk '{print $3}')
 
 if [[ $battery_state == "Charging," ]]; then
-    brightnessctl s 100%
+    brightnessctl --device=acpi_video0 s 100%
 else
     if [[  $battery_percentage != "00%" ]]; then
-        brightnessctl s $battery_percentage
+        brightnessctl --device=acpi_video0 s $battery_percentage
     fi
 fi
