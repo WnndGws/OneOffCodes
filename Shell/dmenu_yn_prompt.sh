19362bb7f67f769da58e69dccf5f857f111eda3c
WG <wynand@gouws.com.au>
2018-04-26 11:40:44 +0800

    Swap yes and no

--- Shell/dmenu_yn_prompt.sh
+++ Shell/dmenu_yn_prompt.sh
@@ -4,6 +4,6 @@
 # For example:
 # `./prompt "Do you want to shutdown?" "shutdown -h now"`
 
-[[ $(echo -e "No\nYes" | dmenu -i -p "$1" -h 40 -fn "Sauce Code Pro Nerd Font Complete Mono:pixelsize=14;0") \
+[[ $(echo -e "Yes\nNo" | dmenu -i -p "$1" -h 40 -fn "Sauce Code Pro Nerd Font Complete Mono:pixelsize=14;0") \
 == "Yes" ]] && zsh -c $2 
 
