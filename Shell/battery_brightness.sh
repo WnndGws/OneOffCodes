9e7dee8357ec4bcdbaca76b528db6920922023bd
WG <wynand@gouws.com.au>
2018-05-15 14:42:18 +0800

    battery percentage is anything bigger than 1

--- Shell/battery_brightness.sh
+++ Shell/battery_brightness.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 ## Sets the display brightness to match battery level
 
-battery_percentage=$(acpi --battery | rg -o '[0-9]{2}%')
+battery_percentage=$(acpi --battery | rg -o '[0-9]+%')
 battery_state=$(acpi --battery | awk '{print $3}')
 
 if [[ $battery_state == "Charging," ]]; then
