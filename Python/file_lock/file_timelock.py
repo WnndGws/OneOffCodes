ab985ce7b399a05280b373a12c90bd7240786339
WG <wynand@gouws.com.au>
2018-06-06 15:19:47 +0800

    Black-ify

--- Python/file_lock/file_timelock.py
+++ Python/file_lock/file_timelock.py
@@ -7,37 +7,38 @@ import os
 import pickle
 
 file_list = {}
-state = 'inputting'
-while state == 'inputting':
-    state = input('Would you like to enter another file? (Y/n): ')
-    if state.lower() in ('yes', '', 'y'):
-        state = 'inputting'
+state = "inputting"
+while state == "inputting":
+    state = input("Would you like to enter another file? (Y/n): ")
+    if state.lower() in ("yes", "", "y"):
+        state = "inputting"
     else:
-        state = 'brokk'
+        state = "brokk"
         break
-    file_input = input('Enter a file you would like to unzip and the date (Filename - yyyymmdd): ')
+    file_input = input(
+        "Enter a file you would like to unzip and the date (Filename - yyyymmdd): "
+    )
     file_name = str(file_input.split(" - ", 1)[0])
     file_date = str(file_input.split(" - ", 1)[1])
     file_list[file_date] = file_name
-    with open('filelock.settings', 'wb') as file:
+    with open("filelock.settings", "wb") as file:
         pickle.dump(file_list, file, protocol=pickle.HIGHEST_PROTOCOL)
 
-current_date = dt.date.today().strftime('%Y%m%d')
+current_date = dt.date.today().strftime("%Y%m%d")
 
-with open('filelock.settings', 'rb') as file:
+with open("filelock.settings", "rb") as file:
     file_list = pickle.load(file)
 
 try:
     target_file = file_list[current_date]
-    os.system('7z x {0}'.format(target_file))
+    os.system("7z x {0}".format(target_file))
 except KeyError:
-    print('No matching files for today')
+    print("No matching files for today")
 
     # FTP into server and unzip file (EASIER OPTION)
     #           OR
     # Unzip file locally and upload onto FTP server
     # whatever one is easier
-
 
     # if __name__ == '__main__':
     #    main()
