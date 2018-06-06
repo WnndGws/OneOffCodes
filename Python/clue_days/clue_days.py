4519cd380f5b92abbfe44dfe24d8a06cc36a5f1f
WG <wynand@gouws.com.au>
2018-06-06 15:19:41 +0800

    Black-ify

--- Python/clue_days/clue_days.py
+++ Python/clue_days/clue_days.py
@@ -9,23 +9,23 @@ import itertools
 import random
 
 numberOfDays = int(input("How many days this month? "))
-diceNumber = ('1', '2', '3', '4', '5', '6')
-diceColour = ('r', 'b')
-cardNumber = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
-cardSuite = ('r', 'b', 'w', 'b')
+diceNumber = ("1", "2", "3", "4", "5", "6")
+diceColour = ("r", "b")
+cardNumber = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+cardSuite = ("r", "b", "w", "b")
 
 dayslist = []
 cardsInDeck = []
 
 for a, b in itertools.product(cardNumber, cardSuite):
-    cardsInDeck.append(a+b)
+    cardsInDeck.append(a + b)
 
 diceRolls = 0
 blackRolls = 0
 redRolls = 0
 while diceRolls < 4:
     roll = random.choice(diceNumber) + random.choice(diceColour)
-    if roll[1] == 'b':
+    if roll[1] == "b":
         blackRolls += int(roll[0])
     else:
         redRolls += int(roll[0])
@@ -34,7 +34,7 @@ while diceRolls < 4:
 redCards = []
 blackCards = []
 for c in cardsInDeck:
-    if c[1] == 'b':
+    if c[1] == "b":
         blackCards.append(c)
     else:
         redCards.append(c)
@@ -63,7 +63,7 @@ while len(dayColours) > 11:
     possibleDeletions = len(dayColours)
     while possibleDeletions > 0:
         c = random.choice(diceColour)
-        if c == 'r':
+        if c == "r":
             possibleDeletions = possibleDeletions - 1
         else:
             del dayColours[possibleDeletions - 1]
