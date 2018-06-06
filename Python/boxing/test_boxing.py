b507e654bf8bb365f4bfe546b299a89528e5486c
WG <wynand@gouws.com.au>
2018-06-06 15:19:38 +0800

    Black-ify

--- Python/boxing/test_boxing.py
+++ Python/boxing/test_boxing.py
@@ -8,18 +8,22 @@ import pytest
 
 import datetime
 
+
 def test_scrape_wikitables():
     result = boxing.scrape_wikitables()
     assert len(result) > 0
 
-@hypothesis.given(a = hypothesis.strategies.datetimes(), b = hypothesis.strategies.integers())
-def test_add_months(a,b):
-    if not 1 < b <12:
+
+@hypothesis.given(
+    a=hypothesis.strategies.datetimes(), b=hypothesis.strategies.integers()
+)
+def test_add_months(a, b):
+    if not 1 < b < 12:
         c = abs(b)
-        c = c - (c//12)*12
+        c = c - (c // 12) * 12
         if (c == 0) or (c == 12):
-            c = c+1
+            c = c + 1
     else:
         c = b
-    result = boxing.add_months(a,b)
-    assert result.month + 12*(result.year - a.year) == a.month + c
+    result = boxing.add_months(a, b)
+    assert result.month + 12 * (result.year - a.year) == a.month + c
