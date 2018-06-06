411292dccf262334253d17003b22cac20ecadde9
WG <wynand@gouws.com.au>
2018-06-06 15:20:33 +0800

    Black-ify

--- Python/reboot_router/reboot_router.py
+++ Python/reboot_router/reboot_router.py
@@ -1,9 +1,9 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-url = 'http://192.168.0.1/html/reboot.html'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
-username = 'admin'
+url = "http://192.168.0.1/html/reboot.html"
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64)"}
+username = "admin"
 password = input("Password? ")
 
 manager = urllib.request.HTTPPasswordMgrWithDefaultRealm()
@@ -19,5 +19,5 @@ req = urllib.request.Request(url, None, headers)
 
 with urllib.request.urlopen(req) as response:
     html = response.read()
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, "html.parser")
     print(soup.prettify())
