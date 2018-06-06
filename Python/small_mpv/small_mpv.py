7c18c5c9bbdf5d561d4c40914da578150f714c9d
WG <wynand@gouws.com.au>
2018-06-06 15:20:38 +0800

    Black-ify

--- Python/small_mpv/small_mpv.py
+++ Python/small_mpv/small_mpv.py
@@ -13,12 +13,12 @@ mpv_leafs = 0
 while mpv_leafs == 0:
     windows_open = i3.get_tree().leaves()
     for leaf in windows_open:
-        if re.match('.*(- mpv)|(Plex).*', leaf.name, re.IGNORECASE) is not None:
+        if re.match(".*(- mpv)|(Plex).*", leaf.name, re.IGNORECASE) is not None:
             mpv_leafs += 1
 
 for leaf in windows_open:
     windows_open = i3.get_tree().leaves()
-    if re.match('.*(- mpv)|(Plex).*', leaf.name, re.IGNORECASE) is not None:
+    if re.match(".*(- mpv)|(Plex).*", leaf.name, re.IGNORECASE) is not None:
         connection_id = leaf.id
         print(connection_id)
         leaf.command(f'[con_id="{connection_id}"] resize set 20 ppt 100 ppt')
