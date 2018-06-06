bffed56dfbb731226dfb6dc79d81d1b4a3c973c6
WG <wynand@gouws.com.au>
2018-06-06 15:13:23 +0800

    Black-ify

--- Python/i3_alternating_windows/i3_alternating_windows.py
+++ Python/i3_alternating_windows/i3_alternating_windows.py
@@ -1,5 +1,5 @@
 #!/bin/python
-'''Run a python script to alternate the order of opening my i3 windows'''
+"""Run a python script to alternate the order of opening my i3 windows"""
 
 import re
 import subprocess
@@ -8,6 +8,7 @@ import sys
 import os
 
 import i3
+
 
 def find_parent(window_id):
     """
@@ -20,13 +21,13 @@ def find_parent(window_id):
         if result[0] is not None:
             return
         for node in n:
-            if node['id'] == window_id:
+            if node["id"] == window_id:
                 result[0] = p
                 return
-            if len(node['nodes']):
-                finder(node['nodes'], node)
+            if len(node["nodes"]):
+                finder(node["nodes"], node)
 
-    finder(root_window['nodes'])
+    finder(root_window["nodes"])
     return result[0]
 
 
@@ -38,18 +39,21 @@ def set_layout():
     """
     current_win = i3.filter(nodes=[], focused=True)
     for win in current_win:
-        parent = find_parent(win['id'])
+        parent = find_parent(win["id"])
 
-        if (parent and "rect" in parent
-                   and parent['layout'] != 'tabbed'
-                   and parent['layout'] != 'stacked'):
-            height = parent['rect']['height']
-            width = parent['rect']['width']
+        if (
+            parent
+            and "rect" in parent
+            and parent["layout"] != "tabbed"
+            and parent["layout"] != "stacked"
+        ):
+            height = parent["rect"]["height"]
+            width = parent["rect"]["width"]
 
             if height > width:
-                new_layout = 'vertical'
+                new_layout = "vertical"
             else:
-                new_layout = 'horizontal'
+                new_layout = "horizontal"
 
             i3.split(new_layout)
 
@@ -58,7 +62,9 @@ def print_help():
     print("Usage: " + sys.argv[0] + " [-p path/to/pid.file]")
     print("")
     print("Options:")
-    print("    -p path/to/pid.file   Saves the PID for this program in the filename specified")
+    print(
+        "    -p path/to/pid.file   Saves the PID for this program in the filename specified"
+    )
     print("")
 
 
@@ -68,7 +74,7 @@ def main():
         changes and call set_layout when focus
         changes
     """
-    opt_list, args = getopt.getopt(sys.argv[1:], 'hp:')
+    opt_list, args = getopt.getopt(sys.argv[1:], "hp:")
     pid_file = None
     for opt in opt_list:
         if opt[0] == "-h":
@@ -78,21 +84,18 @@ def main():
             pid_file = opt[1]
 
     if pid_file:
-        with open(pid_file, 'w') as f:
+        with open(pid_file, "w") as f:
             f.write(str(os.getpid()))
 
-
     process = subprocess.Popen(
-        ['xprop', '-root', '-spy'],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
+        ["xprop", "-root", "-spy"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
-    regex = re.compile(b'^_NET_CLIENT_LIST_STACKING|^_NET_ACTIVE_WINDOW')
+    regex = re.compile(b"^_NET_CLIENT_LIST_STACKING|^_NET_ACTIVE_WINDOW")
 
     last_line = ""
     while True:
         line = process.stdout.readline()
-        if line == b'': #X is dead
+        if line == b"":  # X is dead
             break
         if line == last_line:
             continue
@@ -102,6 +105,7 @@ def main():
 
     process.kill()
     sys.exit()
+
 
 if __name__ == "__main__":
     main()
