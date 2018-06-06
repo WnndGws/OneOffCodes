e4120a41a7ea20ea10da5887e5e0f73cd0e4492b
WG <wynand@gouws.com.au>
2018-06-06 15:20:42 +0800

    Black-ify

--- Python/snap_ya/test_snap_ya.py
+++ Python/snap_ya/test_snap_ya.py
@@ -7,7 +7,8 @@ import hypothesis
 import pytest
 import subprocess
 
+
 def test_package_list():
-    package_list = subprocess.check_output(['trizen', '-Q'])
+    package_list = subprocess.check_output(["trizen", "-Q"])
     package_number = package_list.count(b"\n")
     assert len(snap_ya.get_installed_packages()) == package_number
