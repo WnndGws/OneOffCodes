acf5698a362ed4e4c7f4059ec29a899818025023
WnndGws <WnndGws>
2018-07-28 21:24:28 +0800

    New sleep until

---
+++ Shell/sleep_until.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/zsh
+## Sleep until a certain time
+
+timein=$1
+delimiter=":"
+shutdowntime=$(date -d $1 +%T)
+shutdowntime=$(echo "$(date -d $shutdowntime +%s) + 86400" | bc)
+nowtime=$(date -d now +%s)
+time_ts=$(echo "$shutdowntime - $nowtime" | bc)
+
+while [ $time_ts -gt 0 ]; do
+    echo -ne "Shutdown will occur in $(date -u -d @$time_ts +'%T')......\033[0K\r"
+    sleep 1
+    ((time_ts--))
+done
+# Countdown in terminal
