ffd6b3e6d5110419dc9ba16fb1b24ee410342fe6
WnndGws <WnndGws>
2018-08-02 21:35:53 +0800

    Smaller Size

--- Shell/dmenu_clipboard_management.sh
+++ Shell/dmenu_clipboard_management.sh
@@ -2,7 +2,7 @@
 # Feed this script a link and it will give dmenu
 # some choice programs to use to open it.
 
-x=$(echo -e "clip->primary\nprimary->clip\npaste" | dmenu -h 40 -fn "Sauce Code Pro Nerd Font Complete Mono:pixelsize=14;0" -i -p "Where should I copy to/from?")
+x=$(echo -e "clip->primary\nprimary->clip\npaste" | dmenu -h 40 -fn "CodeNewRoman Nerd Font:pixelsize=15;1" -i -p "Where should I copy to/from?")
 case "$x" in
 	"clip->primary") xsel -ob | xsel -ip ;;
 	"primary->clip") xsel -op | xsel -ib ;;
