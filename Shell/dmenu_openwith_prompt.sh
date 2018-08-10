65f8fa1c8985ed2d400ead9abfa69704d0647f6c
WnndGws <WnndGws>
2018-08-10 14:58:15 +0800

    Add ytdl as option

--- Shell/dmenu_openwith_prompt.sh
+++ Shell/dmenu_openwith_prompt.sh
@@ -11,7 +11,7 @@ visual="$1"
 fi
 echo $visual
 
-x=$(echo -e "umpv_180p\numpv\nmpv\nmpv_loop\nfeh\nfirefox\nparagraph\nsummary" | dmenu -h 40 -fn "CodeNewRoman Nerd Font:pixelsize=15;1" -i -p "How should I open '$visual'?")
+x=$(echo -e "umpv_180p\numpv\nmpv\nmpv_loop\nfeh\nfirefox\nparagraph\nsummary\nytdl" | dmenu -h 40 -fn "CodeNewRoman Nerd Font:pixelsize=15;1" -i -p "How should I open '$visual'?")
 case "$x" in
     summary) clear; rm -f /tmp/para* > /dev/null 2>&1; python $HOME/Git/OneOffCodes/Python/paragraph_scraper/paragraph_scraper.py --url "$1"; python $HOME/Git/OneOffCodes/Python/paragraph_scraper/article_summarise.py; gvim /tmp/para_summarise.txt;;
 	umpv) clear; python $HOME/Git/OneOffCodes/Python/umpv "$1" > /dev/null 2>&1 & disown ;;
@@ -21,5 +21,6 @@ case "$x" in
 	firefox) clear; firefox "$1" 2&>/dev/null & disown ;;
 	feh) clear; rm -rf /tmp/images/* 2>&1 /dev/null; gallery-dl --dest /tmp/images "$1" >/dev/null 2>&1 ; feh --scale-down --recursive /tmp/images --title "%S %n" & disown;;
     paragraph) clear; rm -f /tmp/para* > /dev/null 2>&1; python $HOME/Git/OneOffCodes/Python/paragraph_scraper/paragraph_scraper.py --url "$1"; gvim /tmp/para.txt ;;
+    ytdl) clear; youtube-dl "$1" > /dev/null 2>&1 & ;;
     *) "$x" "$1"
 esac
