8b81be56e86d804817239c3d11cd9c17b38eb664
WnndGws <WnndGws>
2018-08-16 17:01:05 +0800

    Continuous preview

--- Shell/Compile_Latex.sh
+++ Shell/Compile_Latex.sh
@@ -2,8 +2,8 @@
 ## Shell script to compile latex for me using mklatex
 
 tex_file="$1"
-latexmk -xelatex -f -g -silent -cd $tex_file
+latexmk -xelatex -pvc -silent -cd $tex_file
 
 pdf_file=$(echo $tex_file | cut -d'.' -f1)
 pdf_file=$pdf_file".pdf"
-mupdf $pdf_file
+mupdf $pdf_file & disown
