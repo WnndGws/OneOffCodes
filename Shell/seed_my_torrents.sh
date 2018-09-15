5a370cb23a804fa0450713abb1077f7cd4a44d7e
WnndGws <WnndGws>
2018-09-15 16:25:53 +0800

    Download and seed based on .,torrent file name

--- Shell/seed_my_torrents.sh
+++ Shell/seed_my_torrents.sh
@@ -2,6 +2,7 @@
 ## Use aria2c to seed my torrents
 
 for file in $(fd . /home/wynand/wynZFS/Media/ --hidden -e torrent); do
+    file_name=$(echo $file | rev | cut -d'/' -f1 | cut -d'.' -f2- | rev | cut -d'.' -f2-)
     directory=$(dirname $file)
-    aria2c --bt-detach-seed-only --bt-seed-unverified $file --dir $directory > /dev/null 2>&1 & disown
+    aria2c --bt-detach-seed-only --bt-seed-unverified $file --dir $directory --index-out=1=$file_name.mkv > /dev/null 2>&1 & disown
 done
