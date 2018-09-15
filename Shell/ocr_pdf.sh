5e78ce80f5b682582d19b7b2bf4a0d3f8b981ac8
WnndGws <WnndGws>
2018-09-15 16:25:47 +0800

    Simple updates

--- Shell/ocr_pdf.sh
+++ Shell/ocr_pdf.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/zsh
 ## Convert a pdf to tiff, then ocr it
 
-in_file_name=$(echo "$1" | sed 's/\.pdf//')
+full_in_name="$1"
+in_file_name=$(echo "$1" | rev | cut -d'.' -f2- | rev)
 
-convert -density 300 "$1" -depth 8 -strip -background white -alpha off "$in_file_name.tiff"
-tesseract "$in_file_name.tiff" "$in_file_name"_ocr pdf
-rm -rf "$in_file_name.tiff"
+#convert -density 300 "output.pdf" -depth 8 -strip -background white -alpha off "$in_file_name.tiff"
+$HOME/Git/OneOffCodes/Shell/textcleaner -g -e stretch -f 35 -o 15 -a 2 -s 1 "$1" "cleaned.tiff"
+tesseract -l eng "cleaned.tiff" "$in_file_name"_ocr pdf
+rm -rf "cleaned.tiff"
+mupdf "$in_file_name"_ocr.pdf
+#rm -rf output.pdf page*
