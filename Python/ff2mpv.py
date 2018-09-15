8474d7f8854468a482160f9610497f2e190ecc50
WnndGws <WnndGws>
2018-09-15 16:25:13 +0800

    Dont loop

--- Python/ff2mpv.py
+++ Python/ff2mpv.py
@@ -7,7 +7,7 @@ from subprocess import Popen, DEVNULL
 def main():
     message = get_message()
     url = message.get('url')
-    args = ['mpv', '--loop', '--no-terminal', '--', url]
+    args = ['mpv', '--no-terminal', '--', url]
     Popen(args, stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
     # Need to respond something to avoid "Error: An unexpected error occurred"
     # in Browser Console.
