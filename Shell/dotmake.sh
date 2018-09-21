bd41bace08fc9cdb8b396d84be3dc23372536e50
WnndGws <WnndGws>
2018-09-21 14:19:51 +0800

    Reorder to add package list to the list

--- Shell/dotmake.sh
+++ Shell/dotmake.sh
@@ -14,19 +14,19 @@ echo "Pulling from git....."
 cd $dir
 git pull --all
 
+# Get list of installed packages
+echo "Getting list of packages...."
+pacman -Qe | awk '{print $1}' > $HOME/.config/installed_packages/explicit_packages.txt
+awk 'FNR==NR {a[$0]++; next} !a[$0]' $HOME/.config/installed_packages/packages_base.txt $HOME/.config/installed_packages/explicit_packages.txt > $HOME/.config/installed_packages/packages_unique.txt
+sed -i '/xorg-.*/d' $HOME/.config/installed_packages/packages_unique.txt
+sed -i '/xf86-.*/d' $HOME/.config/installed_packages/packages_unique.txt
+rm $HOME/.config/installed_packages/explicit_packages.txt
+
 echo "Updating dotfiles (may take some time)....."
 dotgit decrypt
 dotgit update $computer
 dotgit encrypt
 dotgit generate
-
-# Get list of installed packages
-echo "Getting list of packages...."
-pacman -Qe | awk '{print $1}' > $dir/explicit_packages.txt
-awk 'FNR==NR {a[$0]++; next} !a[$0]' $dir/packages_base.txt $dir/explicit_packages.txt > $dir/packages_unique.txt
-sed -i '/xorg-.*/d' $dir/packages_unique.txt
-sed -i '/xf86-.*/d' $dir/packages_unique.txt
-rm $dir/explicit_packages.txt
 
 git add .
 git commit -S -m "Updated packages" .
