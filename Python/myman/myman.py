9ad5fcd9ec2235543bde6ccef2205ca91e7bcb6f
WnndGws <WnndGws>
2018-09-28 21:28:24 +0800

    Install myman using setuptools

---
+++ Python/myman/myman.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+## Use tldr for man if exits, otherwise rtfm
+
+import click
+import os
+import re
+import subprocess
+
+@click.command()
+@click.option("--program", help="The program who's manual we want to read")
+def try_tldr(program):
+    """Checks to see if there is a tldr"""
+    success = False
+    program_list = subprocess.check_output(["tldr", "--list"]).strip()
+    match = re.findall(r"(%s)" % program, str(program_list))
+    if len(match) > 0:
+        try:
+            args = ["tldr", program]
+            subprocess.check_call(args)
+            success = True
+        except subprocess.CalledProcessError:
+            success = False
+    if not success:
+        try:
+            args = ["man", program]
+            subprocess.check_call(args)
+            success = True
+        except subprocess.CalledProcessError:
+            success = False
+    if not success:
+        try:
+            args = [program, "--help"]
+            subprocess.call(args)
+            success = True
+        except OSError:
+            success = False
+            click.echo(
+                f"There either is no program called {program}, or it does not have a man-page"
+            )
+
+if __name__ == "__main__":
+    try_tldr()
