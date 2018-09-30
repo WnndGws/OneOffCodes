c08a5eb5a1114ce7694092b71d6095d966d28951
WnndGws <WnndGws>
2018-09-30 11:49:45 +0800

    Set explicit flags

--- Shell/seed_single_torrent.sh
+++ Shell/seed_single_torrent.sh
@@ -4,4 +4,4 @@
 file="$1"
 file_name=$(echo $file | rev | cut -d'/' -f1 | cut -d'.' -f2- | rev | cut -d'.' -f2-)
 directory=$(dirname $file)
-aria2c --bt-force-encryption --bt-require-crypto --bt-max-peers=0 --bt-detach-seed-only --bt-seed-unverified $file --dir $directory --index-out=1=$file_name.mkv
+aria2c --bt-force-encryption=true --bt-require-crypto=true --bt-max-peers=0 --bt-detach-seed-only=true --bt-seed-unverified=true $file --dir $directory --index-out=1=$file_name.mkv
