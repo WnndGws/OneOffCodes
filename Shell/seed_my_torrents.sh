cfe2018abf247d33d21a661c0a3c99baff2eb5be
WnndGws <WnndGws>
2018-10-01 15:54:06 +0800

    Limit to single file
    For now

--- Shell/seed_my_torrents.sh
+++ Shell/seed_my_torrents.sh
@@ -4,5 +4,5 @@
 for file in $(fd . /home/wynand/wynZFS/Media/ --hidden -e torrent); do
     file_name=$(echo $file | rev | cut -d'/' -f1 | cut -d'.' -f2- | rev | cut -d'.' -f2-)
     directory=$(dirname $file)
-    aria2c --bt-force-encryption --bt-require-crypto --bt-max-peers=0 --bt-detach-seed-only --bt-seed-unverified $file --dir $directory --index-out=1=$file_name.mkv > /dev/null 2>&1 & disown
+    aria2c --bt-force-encryption --bt-require-crypto --bt-max-peers=0 --bt-detach-seed-only --bt-seed-unverified $file --dir $directory --select-file=1 --index-out=1=$file_name.mkv > /dev/null 2>&1 & disown
 done
