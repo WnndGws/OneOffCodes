9f2d9345aa9c63e73b2d9112393aadeba20f8c97
WnndGws <WnndGws>
2018-10-20 21:57:38 +0800

    Test with cloudflare instead

--- Python/wallpaper_maker/wallpaper_maker.py
+++ Python/wallpaper_maker/wallpaper_maker.py
@@ -17,7 +17,7 @@ import textwrap
 import time
 
 
-def test_internet(host="8.8.8.8", port=53, timeout=3):
+def test_internet(host="1.1.1.1", port=53, timeout=3):
     try:
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
