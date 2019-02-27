6ec39a51293b64cfb5560f7b74003517f2162b74
WnndGws <WnndGws>
2019-02-27 19:33:22 +0800

    ve NBN

--- Shell/dmenu_openwith_prompt.sh
+++ Shell/dmenu_openwith_prompt.sh
@@ -11,11 +11,11 @@ visual="$1"
 fi
 echo $visual
 
-x=$(echo -e "umpv_180p\numpv\nmpv\nmpv_loop\nfeh\nfirefox\nparagraph\nsummary\nytdl\nspeedread" | dmenu -h 40 -fn "CodeNewRoman Nerd Font:pixelsize=15;1" -i -p "How should I open '$visual'?")
+x=$(echo -e "umpv\nmpv\nmpv_loop\nfeh\nfirefox\nparagraph\nsummary\nytdl\nspeedread" | dmenu -h 40 -fn "CodeNewRoman Nerd Font:pixelsize=15;1" -i -p "How should I open '$visual'?")
 case "$x" in
     summary) clear; rm -f /tmp/para* > /dev/null 2>&1; python $HOME/Git/OneOffCodes/Python/paragraph_scraper/paragraph_scraper.py --url "$1"; python $HOME/Git/OneOffCodes/Python/paragraph_scraper/article_summarise.py; gvim /tmp/para_summarise.txt;;
 	umpv) clear; python $HOME/Git/OneOffCodes/Python/umpv "$1" > /dev/null 2>&1 & disown ;;
-	umpv_180p) clear; python $HOME/Git/OneOffCodes/Python/umpv_180p "$1" > /dev/null 2>&1 & disown ;;
+	#umpv_180p) clear; python $HOME/Git/OneOffCodes/Python/umpv_180p "$1" > /dev/null 2>&1 & disown ;;
 	mpv) clear; mpv --ytdl-format="best[height<=720]" --quiet "$1" 2&>/dev/null & disown ;;
 	mpv_loop) clear; mpv -quiet --loop "$1" 2&>/dev/null & disown ;;
 	firefox) clear; firefox "$1" 2&>/dev/null & disown ;;
