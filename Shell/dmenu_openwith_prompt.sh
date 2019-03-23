e2fa5d07996d97170c98a4b1c111a79abbfe3623
WnndGws <WnndGws>
2019-03-23 17:44:36 +0800

    Start paused

--- Shell/dmenu_openwith_prompt.sh
+++ Shell/dmenu_openwith_prompt.sh
@@ -16,7 +16,7 @@ case "$x" in
     summary) clear; rm -f /tmp/para* > /dev/null 2>&1; python $HOME/Git/OneOffCodes/Python/paragraph_scraper/paragraph_scraper.py --url "$1"; python $HOME/Git/OneOffCodes/Python/paragraph_scraper/article_summarise.py; gvim /tmp/para_summarise.txt;;
 	umpv) clear; python $HOME/Git/OneOffCodes/Python/umpv "$1" > /dev/null 2>&1 & disown ;;
 	#umpv_180p) clear; python $HOME/Git/OneOffCodes/Python/umpv_180p "$1" > /dev/null 2>&1 & disown ;;
-	mpv) clear; mpv --ytdl-format="best[height<=720]" --quiet "$1" 2&>/dev/null & disown ;;
+	mpv) clear; mpv --pause --ytdl-format="best[height<=720]" --quiet "$1" 2&>/dev/null & disown ;;
 	mpv_loop) clear; mpv -quiet --loop "$1" 2&>/dev/null & disown ;;
 	firefox) clear; firefox "$1" 2&>/dev/null & disown ;;
 	feh) clear; feh --scale-down --recursive "$1" --title "%S %n" & disown;;
