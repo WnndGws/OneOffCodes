3369266298e16f9d60a2013f9e28bd27d7e383c5
starship <starship@example.com>
2020-03-25 13:46:45 +0800

    Nearly done

--- Python/wallpaper_maker/download_wallpaper_image.py
+++ Python/wallpaper_maker/download_wallpaper_image.py
@@ -4,26 +4,12 @@
 
 import json
 import requests
-import click
 
-@click.command()
-@click.option(
-    "--country",
-    prompt=True,
-    type=click.Choice(
-        ["en-US", "zn-CN", "ja-JP", "en-AU", "en-UK", "de-DE", "en-NZ", "en-CA"]
-    ),
-    default="en-AU",
-    help="Choose the country location of the Bing wallpaper you want to use [DEFAULT: en-AU]",
-)
-@click.option(
-    "--resolution",
-    type=click.Choice(["1920x1200", "1920x1080", "1366x768", "1280x720", "1024x768"]),
-    default="1920x1080",
-    prompt=True,
-    help="Choose the resolution of the image you want to download [DEFAULT: 1920x1080]",
-)
-def download_bing_wallpaper(country, resolution):
+def download_bing_wallpaper():
+
+    country = "en-AU"
+    resolution = "1920x1080"
+
     """Downloads the daily wallpaper from bing as a jpg"""
     # idx determines where to start from. 0 is today, 1 is yesterday, etc.
     idx = "0"
--- Python/wallpaper_maker/manipulate_image.py
+++ Python/wallpaper_maker/manipulate_image.py
@@ -3,40 +3,87 @@
 """
 
 import download_wallpaper_image
-import click
+import get_calendar_events
 
-@click.command()
-@click.option(
-    "--wallpaper_dir",
-    type=click.Path(),
-    default="~/Pictures",
-    help="Path to the wallpaper directory [DEFAULT: ~/Pictures]",
-    )
-@click.option(
-    "--quote_file",
-    type=click.Path(),
-    default="~/.config/wallpaperMaker/quotes.txt",
-    help="Path to the newline separated quotes file",
-    )
-@click.option(
-    "--font",
-    type=click.Path(),
-    default="/usr/share/fonts/TTF/DejaVuSansMono.ttf",
-    help="Path to the .ttf font file [DEFAULT: DejaVuSansMono]",
-    )
-@click.option("--font-size", default=50, help="Font size [DEFAULT: 50]")
-@click.option(
-    "--bing",
-    is_flag=True,
-    help="Use this flag if you want to use the daily Bing wallpaper instead of a local im
-    )
-@click.option(
-    "--agenda",
-    is_flag=True,
-    help="Use if want to append your day's agenda to the quote",
-    )
-def mannipulate_wallpaper(wallpaper_dir, quote_file, font, font_size, bing, agenda):
+import random
+import textwrap
+from PIL import Image, ImageDraw, ImageFont, ImageStat
+
+import pdb
+
+def manipulate_wallpaper():
+
+    pdb.set_trace()
+
+    quote_file = "./quotes.txt"
+    base_image = Image.open("/tmp/bing.jpg").convert("RGBA")
+
     #set font
+    font = "/usr/share/fonts/TTF/Hack Regular Nerd Font Complete Mono.ttf"
+    font_size = 50
     quote_font = ImageFont.truetype(font, font_size)
 
     #get image
+    download_wallpaper_image.download_bing_wallpaper()
+
+    #get agenda
+    agenda = get_calendar_events.main()
+
+    # make a blank image for the text, initialized to transparent text color
+    text_image = Image.new("RGBA", base_image.size, (255, 255, 255, 0))
+    with open(quote_file) as f:
+        quote_pool = f.read().splitlines()
+    random_quote = random.choice(quote_pool)
+    quote_lines = textwrap.wrap(random_quote, width=60)
+    quote_lines = quote_lines + "/n" + agenda
+
+    # get a drawing context
+    draw = ImageDraw.Draw(text_image)
+
+    # determine location of text
+    x_loc = base_image.size[0]
+
+    # determine the size of one line of the quote, and multiply by how many lines giving the y-size
+    quote_size_y = len(quote_lines) * quote_font.getsize(quote_lines[0])[1]
+
+    # determine x size by seeing how wide the text will be
+    quote_size_x = quote_font.getsize(quote_lines[0])[0]
+
+    # put the quote so the centre always matches the centre of the image
+    y_loc = base_image.size[1] / 2 - (quote_size_y / 2)
+
+    # get average background image brightness by converting to greyscale and getting RMS
+    mean_brightness = int(ImageStat.Stat(base_image.convert("L")).mean[0])
+    square_brightness = int(100 + mean_brightness)
+
+    # draw text
+    for line in quote_lines:
+        line_width, line_height = quote_font.getsize(line)
+        draw.text(
+            ((x_loc - line_width - 20), y_loc),
+            line,
+            font=quote_font,
+            fill=((255, 255, 255, square_brightness)),
+        )
+        y_loc += line_height
+    textbox_image = Image.new("RGBA", base_image.size, (255, 255, 255, 0))
+    draw = ImageDraw.Draw(textbox_image)
+    x_loc = base_image.size[0]
+    y_loc = base_image.size[1] / 2 - (quote_size_y / 2)
+    draw.rectangle(
+        (
+            (x_loc - quote_size_x * 1.5),
+            y_loc - 10,
+            x_loc - 10,
+            y_loc + quote_size_y + 10,
+        ),
+        (0, 0, 0, square_brightness),
+    )
+
+    image_out = Image.alpha_composite(base_image, textbox_image)
+    image_out = Image.alpha_composite(image_out, text_image)
+
+    image_out.save("/tmp/wallpaper.png")
+
+if __name__ == "__main__":
+    manipulate_wallpaper()
