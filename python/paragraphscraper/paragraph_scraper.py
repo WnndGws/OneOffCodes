ab8b48fe2758788fe779c9b765bedf9a5c6659c7
WnndGws <WnndGws@users.noreply.github.com>
2020-05-21 04:01:52 +0800

    Run through black

--- python/paragraphscraper/paragraph_scraper.py
+++ python/paragraphscraper/paragraph_scraper.py
@@ -7,19 +7,16 @@ import requests_html
 
 
 @click.command()
-@click.option(
-    "--url",
-    prompt=True,
-    help="The URL of the website to check"
-)
+@click.option("--url", prompt=True, help="The URL of the website to check")
 def get_paragraph(url):
-    # Get the content into my memory
+    """ Get the content into my memory """
     page = requests_html.HTMLSession().get(url)
-    paragraphs = page.html.find('p')
+    paragraphs = page.html.find("p")
 
     with open("/tmp/para.txt", "a") as f:
         for p in paragraphs:
-            f.write(f'\n {p.text}')
+            f.write(f"\n {p.text}")
+
 
 if __name__ == "__main__":
     get_paragraph()
