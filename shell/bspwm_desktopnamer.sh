b1fd46f2a103116aa7e355cac09f5c9f7c9d6ce2
WnndGws <WnndGws@users.noreply.github.com>
2020-05-20 16:52:49 +0800

    Also rename if move a window to a workspace

--- shell/bspwm_desktopnamer.sh
+++ shell/bspwm_desktopnamer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 ## Script to use bspc subscribe to constantly watch windows
 
-bspc subscribe node_{add,remove} | while read -r line; do
-    $HOME/Git/scripts/python/bspwm_desktopnamer
+bspc subscribe node_{add,remove,transfer} | while read -r line; do
+    "$HOME/Git/scripts/python/bspwm_desktopnamer"
 done
