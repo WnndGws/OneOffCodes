467789c1acb9f0fd9195fb4da238dbb26330709d
WnndGws <WnndGws@users.noreply.github.com>
2020-05-20 16:51:54 +0800

    clean up a bit

--- python/paragraphscraper/paragraph_scraper.py
+++ python/paragraphscraper/paragraph_scraper.py
@@ -1,36 +1,25 @@
-#!/bin/python
-## Takes the argument of a url and outputs the paragraph text from that page to a textfile
+#!/usr/bin/python3
+""" Re-written to scrape paragraph tags on a website
+"""
 
-import re
-import requests
-
-from bs4 import BeautifulSoup
-
-## Collect command-line arguments from input
-def getopts(argv):
-    opts = {}  # Empty dictionary to store key-value pairs.
-    while argv:  # While there are arguments left to parse...
-        if argv[0][0] == "-":  # Found a "-name value" pair.
-            opts[argv[0]] = argv[1]  # Add key and value to the dictionary.
-        argv = argv[1:]  # Reduce the argument list by copying it starting from index 1.
-    return opts
+import click
+import requests_html
 
 
-def scrape_content(url):
-    s = requests.Session()
-    req = s.get(url, allow_redirects=True)
-    if req.status_code == 200:
-        soup = BeautifulSoup(req.content, "html.parser")
-        with open("/tmp/para.txt", "a") as f:
-            for words in soup.findAll("p"):
-                f.write(f"\n\n {words.text}")
-            f.close()
+@click.command()
+@click.option(
+    "--url",
+    prompt=True,
+    help="The URL of the website to check"
+)
+def get_paragraph(url):
+    # Get the content into my memory
+    page = requests_html.HTMLSession().get(url)
+    paragraphs = page.html.find('p')
 
+    with open("/tmp/para.txt", "a") as f:
+        for p in paragraphs:
+            f.write(f'\n {p.text}')
 
 if __name__ == "__main__":
-    from sys import argv
-
-    myargs = getopts(argv)
-    if "--url" in myargs:
-        url = myargs["--url"]
-    scrape_content(url)
+    get_paragraph()
