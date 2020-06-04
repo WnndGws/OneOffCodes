86089600a5475a5f584433e9cffb8004529463d0
WnndGws <WnndGws@users.noreply.github.com>
2020-06-05 00:19:07 +0800

    Hacked together for now

--- shell/bspwm_output_desktops_to_sxhkd.sh
+++ shell/bspwm_output_desktops_to_sxhkd.sh
@@ -2,7 +2,10 @@
 ## Runs ./bspwm_get_desktops and appends them to the sxhkdrc
 
 # Delete the last 3 lines of the file
-sed -i '$d' "$XDG_CONFIG_HOME/sxhkd/sxhkdrc" | sed '$d' | sed '$d'
+sed -i '$d' "$XDG_CONFIG_HOME/sxhkd/sxhkdrc" &&
+sed -i '$d' "$XDG_CONFIG_HOME/sxhkd/sxhkdrc" &&
+sed -i '$d' "$XDG_CONFIG_HOME/sxhkd/sxhkdrc" &&
+sed -i '$d' "$XDG_CONFIG_HOME/sxhkd/sxhkdrc"
 
 # Append output of script to the file
 $HOME/git/scripts/shell/bspwm_output_desktops.sh >> "$XDG_CONFIG_HOME/sxhkd/sxhkdrc"
