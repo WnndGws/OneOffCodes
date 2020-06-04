63919f9d599cf7138e9601a5c0b3e36bae37b02e
WnndGws <WnndGws@users.noreply.github.com>
2020-06-05 00:15:03 +0800

    Script to handle appending text to config

---
+++ shell/bspwm_output_desktops_to_sxhkd.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+## Runs ./bspwm_get_desktops and appends them to the sxhkdrc
+
+# Delete the last 3 lines of the file
+sed -i '$d' "$XDG_CONFIG_HOME/sxhkd/sxhkdrc" | sed '$d' | sed '$d'
+
+# Append output of script to the file
+$HOME/git/scripts/shell/bspwm_output_desktops.sh >> "$XDG_CONFIG_HOME/sxhkd/sxhkdrc"
