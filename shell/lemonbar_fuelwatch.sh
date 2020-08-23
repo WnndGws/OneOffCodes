12e67618e06d79350a356d6a9038b2edcaaa7363
WnndGws <WnndGws@users.noreply.github.com>
2020-08-23 13:06:04 +0800

    Remove unused things

--- shell/lemonbar_fuelwatch.sh
+++ shell/lemonbar_fuelwatch.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 # Outputs cheapest fuel today and tomorrow, allowing me to make a choice
 
-red='#65350'
-green='#99c76c'
-yellow='#ffc24b'
-
 while true; do
     today_price=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake" | rg --only-matching --pcre2 "(?<=<price>)\d{3}\.\d{1}(?=<\/price>)" | head -n 1)
-    today_location=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake" | rg --only-matching --pcre2 "(?<=<trading-name>).*?(?=<\/trading-name>)" | head -n 1)
+    #today_location=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake" | rg --only-matching --pcre2 "(?<=<trading-name>).*?(?=<\/trading-name>)" | head -n 1)
     tomorrow_price=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake&Day=tomorrow" | rg --only-matching --pcre2 "(?<=<price>)\d{3}\.\d{1}(?=<\/price>)" | head -n 1)
-    tomorrow_location=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake&Day=tomorrow" | rg --only-matching --pcre2 "(?<=<trading-name>).*?(?=<\/trading-name>)" | head -n 1)
+    #tomorrow_location=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake&Day=tomorrow" | rg --only-matching --pcre2 "(?<=<trading-name>).*?(?=<\/trading-name>)" | head -n 1)
 
     #echo "F$today_location($today_price) $tomorrow_location($tomorrow_price)"
-    echo "F $today_price  $tomorrow_price"
+    [ -z "$tomorrow_price" ] && echo "F $today_price" || echo "F $today_price  $tomorrow_price"
     sleep 43200
 done
