96f1dc0ec35b353775c62aa51f078b8a1f6610b5
WnndGws <WnndGws@users.noreply.github.com>
2020-08-24 00:23:46 +0800

    'Cache' results instead of wget-ing twice

--- shell/lemonbar_fuelwatch.sh
+++ shell/lemonbar_fuelwatch.sh
@@ -2,10 +2,12 @@
 # Outputs cheapest fuel today and tomorrow, allowing me to make a choice
 
 while true; do
-    today_price=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake" | rg --only-matching --pcre2 "(?<=<price>)\d{3}\.\d{1}(?=<\/price>)" | head -n 1)
-    #today_location=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake" | rg --only-matching --pcre2 "(?<=<trading-name>).*?(?=<\/trading-name>)" | head -n 1)
-    tomorrow_price=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake&Day=tomorrow" | rg --only-matching --pcre2 "(?<=<price>)\d{3}\.\d{1}(?=<\/price>)" | head -n 1)
-    #tomorrow_location=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake&Day=tomorrow" | rg --only-matching --pcre2 "(?<=<trading-name>).*?(?=<\/trading-name>)" | head -n 1)
+    raw_output_today=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake")
+    raw_output_tomorrow=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake&Day=tomorrow")
+    today_price=$(printf '%s' '$raw_output_today' | rg --only-matching --pcre2 "(?<=<price>)\d{3}\.\d{1}(?=<\/price>)" | head -n 1)
+    #today_location=$(printf '%s' '$raw_output_today' | rg --only-matching --pcre2 "(?<=<trading-name>).*?(?=<\/trading-name>)" | head -n 1)
+    tomorrow_price=$(printf '%s' '$raw_output_tomorrow' | rg --only-matching --pcre2 "(?<=<price>)\d{3}\.\d{1}(?=<\/price>)" | head -n 1)
+    #tomorrow_location=$(printf '%s' '$raw_output_tomorrow' | rg --only-matching --pcre2 "(?<=<trading-name>).*?(?=<\/trading-name>)" | head -n 1)
 
     #echo "F$today_location($today_price) $tomorrow_location($tomorrow_price)"
     [ -z "$tomorrow_price" ] && echo "F $today_price" || echo "F $today_price  $tomorrow_price"
