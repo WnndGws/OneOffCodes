184ed52b4aa50641221dbc0e10985f67f0704b08
WnndGws <WnndGws@users.noreply.github.com>
2020-09-21 01:20:38 +0800

    Need to cut off last 2 characters to handle 2 character names

--- shell/bspwm_output_desktops.sh
+++ shell/bspwm_output_desktops.sh
@@ -3,6 +3,7 @@
 
 desktop_head="$(bspc query --desktops | head -n1)"
 desktop_head="${desktop_head%?}" # Split off last character
+desktop_head="${desktop_head%?}" # Split off last character again
 all_desktops="$(bspc query --desktops)"
 
 manipulated_string="<++>"
