a25ae6e000e810c6c282ac58f30b6ddeee7c17be
WnndGws <WnndGws@users.noreply.github.com>
2021-01-17 15:33:35 +0800

    New gpu is AMD

--- shell/lemonbar_gpu.sh
+++ shell/lemonbar_gpu.sh
@@ -1,13 +1,14 @@
-#!/usr/bin/env sh
-# Checks how many packages need updating
+#!/usr/bin/env zsh
+# Checks GPU stats
 
 #Need to update or it wont know there are new packages
 while true; do
     # Get 10th row, 3rd collumn and strip last character
-    temp=$(nvidia-smi | awk 'NR==10 {print substr($3, 1, length($3)-1)}')
-    usage=$(nvidia-smi | awk 'NR==10 {print substr($13, 1, length($13))}')
+    temp=$(sensors | rg --after-context 3 "radeon" | awk 'NR==3 {print substr($2, 2, length($2)-5)}')
+    usage=$(radeontop --dump - | rg --only-matching --pcre2 --max-count 1 "(?<=gpu )\d\.\d{2}")
+    usage=$(printf "%.02d" $usage)
 
-    echo "G $usage ($temp°C)"
+    echo "G $usage% ($temp°C)"
 
     sleep 2
 done
