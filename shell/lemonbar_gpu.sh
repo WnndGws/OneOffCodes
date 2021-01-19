50214884f761439dca93f48f436c4f09aedcdc5e
WnndGws <WnndGws@users.noreply.github.com>
2021-01-19 16:47:36 +0800

    Updated from ati to amdgpu

--- shell/lemonbar_gpu.sh
+++ shell/lemonbar_gpu.sh
@@ -3,8 +3,8 @@
 
 #Need to update or it wont know there are new packages
 while true; do
-    # Get 10th row, 3rd collumn and strip last character
-    temp=$(sensors | rg --after-context 3 "radeon" | awk 'NR==3 {print substr($2, 2, length($2)-5)}')
+    # Get 4 row, 2rd collumn and strip last character
+    temp=$(sensors | rg --after-context 3 "amdgpu" | awk 'NR==4 {print substr($2, 2, length($2)-5)}')
     usage=$(radeontop --dump - | rg --only-matching --pcre2 --max-count 1 "(?<=gpu )\d\.\d{2}")
     usage=$(printf "%2d" $usage)
 
