23a387daf87840d319ed2ff63c59ec0f36bd5471
Jason <ammgws@users.noreply.github.com>
2021-01-19 22:09:30 +0900

    Fix spelling

--- shell/lemonbar_gpu.sh
+++ shell/lemonbar_gpu.sh
@@ -3,7 +3,7 @@
 
 #Need to update or it wont know there are new packages
 while true; do
-    # Get 4 row, 2rd collumn and strip last character
+    # Get 4th row, 2nd column and strip last character
     temp=$(sensors | rg --after-context 3 "amdgpu" | awk 'NR==4 {print substr($2, 2, length($2)-5)}')
     usage=$(radeontop --dump - | rg --only-matching --pcre2 --max-count 1 "(?<=gpu )\d\.\d{2}")
     usage=$(printf "%2d" $usage)
