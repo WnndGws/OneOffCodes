7d5ae8ed0593bad8917a85ceca043411b2420a4a
Jason <ammgws@users.noreply.github.com>
2021-01-21 21:19:55 +0900

    Change formatting to match other files

--- shell/lemonbar_check_sshuttle.sh
+++ shell/lemonbar_check_sshuttle.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-##Checks that my external ip is sshuttle
+## Checks that my external ip is sshuttle
 
 while true; do
     ip_head=$(curl -s zx2c4.com/ip | head -n1 | cut -d'.' -f1)
