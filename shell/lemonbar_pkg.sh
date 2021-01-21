0d23328f06f6d845e274d06dd939e334bca62c2d
Jason <ammgws@users.noreply.github.com>
2021-01-21 21:21:17 +0900

    Change formatting to match other files

--- shell/lemonbar_pkg.sh
+++ shell/lemonbar_pkg.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
-# Checks how many packages need updating
+## Checks how many packages need updating
 
-#Need to update or it wont know there are new packages
+# Need to update or it wont know there are new packages
 
 # TODO; test if works with empty list
 ignore_list="(^$(rg "^Ignore.*" /etc/pacman.conf | sed -e 's/^.*= //' -e 's/ /$)|(^/g')$)"
@@ -13,7 +13,7 @@ while true; do
     [ -z $pac ] && pac=0
 
     aur=$(pikaur -Qqu 2> /dev/null | rg --invert-match --count "$ignore_list")
-    ## Need to minus 1 since pikaur outputs a non-blank blank line
+    # Need to minus 1 since pikaur outputs a non-blank blank line
     #aur=$(awk -v a="$aur" 'BEGIN { printf a-1 }')
     aur=$(awk -v a="$aur" -v p="$pac" 'BEGIN { printf a-p }')
 
