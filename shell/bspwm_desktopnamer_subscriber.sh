06de5fc84b91ea840fa049fd6e3131fd365f0079
Jason <ammgws@users.noreply.github.com>
2021-01-21 21:32:04 +0900

    Fix comment formatting

--- shell/bspwm_desktopnamer_subscriber.sh
+++ shell/bspwm_desktopnamer_subscriber.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-##Subscribes to bspc and runs script when needed
+## Subscribes to bspc and runs script when needed
 
 bspc subscribe node_add node_remove node_transfer node_focus | while read -r _; do
     /home/wynand/git/scripts/shell/bspwm_desktopnamer.sh
