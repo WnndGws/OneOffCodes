e9f1fbc4dc1d19aca0aa76432dd6ab5b82b73155
Jason <ammgws@users.noreply.github.com>
2021-01-21 21:18:12 +0900

    Change servo area

--- shell/lemonbar_fuelwatch.sh
+++ shell/lemonbar_fuelwatch.sh
@@ -2,8 +2,8 @@
 # Outputs cheapest fuel today and tomorrow, allowing me to make a choice
 
 while true; do
-    raw_output_today=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake")
-    raw_output_tomorrow=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Bibra%20Lake&Day=tomorrow")
+    raw_output_today=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Coolbellup")
+    raw_output_tomorrow=$(wget -qO- "http://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=1&Suburb=Coolbellup&Day=tomorrow")
     today_price=$(printf "%s" "$raw_output_today" | rg --only-matching --pcre2 "(?<=<price>)\d{3}\.\d{1}(?=<\/price>)" | head -n 1)
     #today_location=$(printf "%s" "$raw_output_today" | rg --only-matching --pcre2 "(?<=<trading-name>).*?(?=<\/trading-name>)" | head -n 1)
     tomorrow_price=$(printf "%s" "$raw_output_tomorrow" | rg --only-matching --pcre2 "(?<=<price>)\d{3}\.\d{1}(?=<\/price>)" | head -n 1)
