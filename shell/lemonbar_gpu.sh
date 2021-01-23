e98508bbe50e46597ab5247fdb5cbe4db44aafd7
WnndGws <WnndGws@users.noreply.github.com>
2021-01-23 18:36:48 +0800

    Find one OR MORE numbers rather than just one number

--- shell/lemonbar_gpu.sh
+++ shell/lemonbar_gpu.sh
@@ -4,7 +4,7 @@
 while true; do
     # Get 4th row, 2nd column and strip last character
     temp=$(sensors | rg --after-context 3 "amdgpu" | awk 'NR==4 {print substr($2, 2, length($2)-5)}')
-    usage=$(radeontop --dump - | rg --only-matching --pcre2 --max-count 1 "(?<=gpu )\d\.\d{2}")
+    usage=$(radeontop --dump - | rg --only-matching --pcre2 --max-count 1 "(?<=gpu )\d+\.\d{2}")
     usage=$(printf "%2d" $usage)
 
     echo "G $usage% ($temp°C)"
